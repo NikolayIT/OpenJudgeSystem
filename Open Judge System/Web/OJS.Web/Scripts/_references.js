0f617b63ef2b59e3b8d602ec10b2a677e6c1309c
NikolayIT <admin@nikolay.it>
2013-12-12 15:10:28 +0200

    Updated modernizr to version 2.7.1 (old version 2.6.2)

--- Open Judge System/Web/OJS.Web/Scripts/_references.js
+++ Open Judge System/Web/OJS.Web/Scripts/_references.js
@@ -1,3 +1,3 @@
-﻿/// <reference path="jquery-2.0.3.js" />
-/// <reference path="modernizr-2.6.2.js" />
+﻿/// <reference path="modernizr-2.7.1.js" />
+/// <reference path="jquery-2.0.3.js" />
 /// <reference path="bootstrap.js" />
