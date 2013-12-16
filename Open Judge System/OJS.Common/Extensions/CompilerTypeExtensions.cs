a58a67e5fba31b286a304aec68093d83448b3e60
NikolayIT <admin@nikolay.it>
2013-12-16 17:48:10 +0200

    Added get file extension methods to CompilerType and and ExecutionStrategyType

---
+++ Open Judge System/OJS.Common/Extensions/CompilerTypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace OJS.Common.Extensions
+{
+    using System;
+
+    using OJS.Common.Models;
+
+    public static class CompilerTypeExtensions
+    {
+        public static string GetFileExtension(this CompilerType compilerType)
+        {
+            switch (compilerType)
+            {
+                case CompilerType.None:
+                    return null;
+                case CompilerType.CSharp:
+                    return "cs";
+                case CompilerType.MsBuild:
+                    return "zip";
+                case CompilerType.CPlusPlusGcc:
+                    return "cpp";
+                case CompilerType.Java:
+                    return "java";
+                default:
+                    throw new ArgumentOutOfRangeException("compilerType");
+            }
+        }
+    }
+}
---
+++ Open Judge System/OJS.Common/Extensions/ExecutionStrategyTypeExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace OJS.Common.Extensions
+{
+    using System;
+
+    using OJS.Common.Models;
+
+    public static class ExecutionStrategyTypeExtensions
+    {
+        public static string GetFileExtension(this ExecutionStrategyType executionStrategyType)
+        {
+            switch (executionStrategyType)
+            {
+                case ExecutionStrategyType.CompileExecuteAndCheck:
+                    return null; // The file extension depends on the compiler.
+                case ExecutionStrategyType.NodeJsPreprocessExecuteAndCheck:
+                    return "js";
+                default:
+                    throw new ArgumentOutOfRangeException("executionStrategyType");
+            }
+        }
+    }
+}
