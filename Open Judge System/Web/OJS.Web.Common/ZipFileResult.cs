914de54cb1943ff35e7abad308420a47ac637a4d
NikolayIT <admin@nikolay.it>
2013-12-16 17:01:21 +0200

    Added ZipFileResult action result for returning Ionic zip files

---
+++ Open Judge System/Web/OJS.Web.Common/ZipFileResult.cs
@@ -0,0 +1,43 @@
+﻿namespace OJS.Web.Common
+{
+    using System.Web.Mvc;
+
+    using Ionic.Zip;
+
+    /// <summary>
+    /// A content result which can accepts a DotNetZip ZipFile object to write to the output stream
+    /// As seen on http://whatschrisdoing.com/blog/2010/03/22/asp-net-mvc-and-ionic-zip/
+    /// </summary>
+    public class ZipFileResult : ActionResult
+    {
+        private readonly ZipFile zip;
+
+        private readonly string filename;
+
+        public ZipFileResult(ZipFile zip)
+        {
+            this.zip = zip;
+            this.filename = null;
+        }
+
+        public ZipFileResult(ZipFile zip, string filename)
+        {
+            this.zip = zip;
+            this.filename = filename;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var response = context.HttpContext.Response;
+
+            response.ContentType = "application/zip";
+            response.AddHeader(
+                "Content-Disposition",
+                "attachment;" + (string.IsNullOrEmpty(this.filename) ? string.Empty : "filename=" + this.filename));
+            
+            this.zip.Save(response.OutputStream);
+
+            response.End();
+        }
+    }
+}
