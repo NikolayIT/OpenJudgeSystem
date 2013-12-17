191e2e19235b800d0ea1d74b3b5a08ecfde011dd
NikolayIT <admin@nikolay.it>
2013-12-17 16:40:55 +0200

    Added extension methods IsAdmin and IsLoggedIn to IPrincipal (#317) Included namespace OJS.Web.Common to all razor views

---
+++ Open Judge System/Web/OJS.Web.Common/PrincipalExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace OJS.Web.Common
+{
+    using System.Security.Principal;
+
+    public static class PrincipalExtensions
+    {
+        public static bool IsLoggedIn(this IPrincipal principal)
+        {
+            return principal.Identity.IsAuthenticated;
+        }
+
+        public static bool IsAdmin(this IPrincipal principal)
+        {
+            return principal.IsInRole("Administrator");
+        }
+    }
+}
