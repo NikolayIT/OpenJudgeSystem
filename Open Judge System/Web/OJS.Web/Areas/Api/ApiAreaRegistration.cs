99dcf6f66fdbda9378bed83c7a26e8dee6220f56
NikolayIT <admin@nikolay.it>
2014-01-13 16:39:53 +0200

    Added empty "Api" area

---
+++ Open Judge System/Web/OJS.Web/Areas/Api/ApiAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace OJS.Web.Areas.Api
+{
+    public class ApiAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Api";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Api_default",
+                "Api/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
