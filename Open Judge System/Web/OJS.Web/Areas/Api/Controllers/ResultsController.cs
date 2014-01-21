c9f6b4beba0046a47917574e7db456c040c68fc1
NikolayIT <admin@nikolay.it>
2014-01-21 19:03:42 +0200

    Implemented results API. Closes #395

---
+++ Open Judge System/Web/OJS.Web/Areas/Api/Controllers/ResultsController.cs
@@ -0,0 +1,97 @@
+﻿namespace OJS.Web.Areas.Api.Controllers
+{
+    using System.Globalization;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    using OJS.Common;
+    using OJS.Data;
+
+    // TODO: Introduce class ApiController
+    public class ResultsController : Controller
+    {
+        public ResultsController(IOjsData data)
+        {
+            this.Data = data;
+        }
+
+        protected IOjsData Data { get; set; }
+
+        // TODO: Extract method from these two methods since 90% of their code is the same
+        public ContentResult GetPointsByAnswer(string apiKey, int? contestId, string answer)
+        {
+            if (string.IsNullOrWhiteSpace(apiKey) || string.IsNullOrWhiteSpace(answer) || !contestId.HasValue)
+            {
+                return this.Content(string.Format("ERROR: Invalid arguments"));
+            }
+
+            var user = this.Data.Users.GetById(apiKey);
+            if (user == null || user.Roles.All(x => x.Role.Name != GlobalConstants.AdministratorRoleName))
+            {
+                return this.Content(string.Format("ERROR: Invalid API key"));
+            }
+
+            var participants = this.Data.Participants.All().Where(
+                x => x.IsOfficial && x.ContestId == contestId.Value && x.Answers.Any(a => a.Answer == answer));
+
+            var participant = participants.FirstOrDefault();
+            if (participant == null)
+            {
+                return this.Content(string.Format("ERROR: No participants found"));
+            }
+
+            if (participants.Count() > 1)
+            {
+                return this.Content(string.Format("ERROR: More than one participants found"));
+            }
+
+            var points =
+                participant.Contest.Problems.Select(
+                    problem =>
+                    problem.Submissions.Where(z => z.ParticipantId == participant.Id)
+                        .OrderByDescending(z => z.Points)
+                        .Select(z => z.Points)
+                        .FirstOrDefault()).Sum(x => x);
+
+            return this.Content(points.ToString(CultureInfo.InvariantCulture));
+        }
+
+        public ContentResult GetPointsByEmail(string apiKey, int? contestId, string email)
+        {
+            if (string.IsNullOrWhiteSpace(apiKey) || string.IsNullOrWhiteSpace(email) || !contestId.HasValue)
+            {
+                return this.Content(string.Format("ERROR: Invalid arguments"));
+            }
+
+            var user = this.Data.Users.GetById(apiKey);
+            if (user == null || user.Roles.All(x => x.Role.Name != GlobalConstants.AdministratorRoleName))
+            {
+                return this.Content(string.Format("ERROR: Invalid API key"));
+            }
+
+            var participants = this.Data.Participants.All().Where(
+                x => x.IsOfficial && x.ContestId == contestId.Value && x.User.Email == email);
+
+            var participant = participants.FirstOrDefault();
+            if (participant == null)
+            {
+                return this.Content(string.Format("ERROR: No participants found"));
+            }
+
+            if (participants.Count() > 1)
+            {
+                return this.Content(string.Format("ERROR: More than one participants found"));
+            }
+
+            var points =
+                participant.Contest.Problems.Select(
+                    problem =>
+                    problem.Submissions.Where(z => z.ParticipantId == participant.Id)
+                        .OrderByDescending(z => z.Points)
+                        .Select(z => z.Points)
+                        .FirstOrDefault()).Sum(x => x);
+
+            return this.Content(points.ToString(CultureInfo.InvariantCulture));
+        }
+    }
+}
