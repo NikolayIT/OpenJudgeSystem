5a426d7618650fc64f9d64315ec11d0324621c8e
NikolayIT <admin@nikolay.it>
2014-02-07 14:07:18 +0200

    jQuery updated to version 2.1.0 (#422)

--- Open Judge System/Web/OJS.Web/Scripts/_references.js
+++ Open Judge System/Web/OJS.Web/Scripts/_references.js
@@ -1,3 +1,3 @@
-﻿/// <reference path="modernizr-2.7.1.js" />
-/// <reference path="jquery-2.0.3.js" />
+﻿/// <reference path="jquery-2.1.0.js" />
+/// <reference path="modernizr-2.7.1.js" />
 /// <reference path="bootstrap.js" />
