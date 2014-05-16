5533316e8e7ee50c53a8818c28823dfd506492d6
NikolayIT <admin@nikolay.it>
2014-05-16 17:24:09 +0300

    Added StandardProcessExecutor (not tested, yet)

---
+++ Open Judge System/Workers/OJS.Workers.Executors/StandardProcessExecutor.cs
@@ -0,0 +1,168 @@
+﻿namespace OJS.Workers.Executors
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using log4net;
+
+    using OJS.Workers.Common;
+    using OJS.Workers.Executors.Process;
+
+    public class StandardProcessExecutor : IExecutor
+    {
+        private static ILog logger;
+
+        public StandardProcessExecutor()
+        {
+            logger = LogManager.GetLogger(typeof(StandardProcessExecutor));
+            //// logger.Info("Initialized.");
+        }
+
+        public ProcessExecutionResult Execute(string fileName, string inputData, int timeLimit, int memoryLimit, IEnumerable<string> executionArguments = null)
+        {
+            var result = new ProcessExecutionResult { Type = ProcessExecutionResultType.Success };
+            var workingDirectory = new FileInfo(fileName).DirectoryName;
+
+            var commandLineBuilder = new StringBuilder();
+            foreach (var argument in executionArguments)
+            {
+                commandLineBuilder.Append(' ');
+                commandLineBuilder.Append(argument);
+            }
+
+            var process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = fileName;
+            process.StartInfo.WorkingDirectory = new FileInfo(fileName).Directory.ToString();
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.ErrorDialog = false;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+
+            // Write to standard input using another thread
+            process.StandardInput.WriteLineAsync(inputData).ContinueWith(
+                delegate
+                    {
+                        // ReSharper disable once AccessToDisposedClosure
+                        process.StandardInput.FlushAsync().ContinueWith(
+                            delegate
+                                {
+                                    process.StandardInput.Close();
+                                });
+                    });
+
+            // Read standard output using another thread to prevent process locking (waiting us to empty the output buffer)
+            var processOutputTask = process.StandardOutput.ReadToEndAsync().ContinueWith(
+                x =>
+                {
+                    result.ReceivedOutput = x.Result;
+                });
+
+            // Read standard error using another thread
+            var errorOutputTask = process.StandardError.ReadToEndAsync().ContinueWith(
+                x =>
+                {
+                    result.ErrorOutput = x.Result;
+                });
+
+            // Read memory consumption every few milliseconds to determine the peak memory usage of the process
+            const int TimeIntervalBetweenTwoMemoryConsumptionRequests = 45;
+            var memoryTaskCancellationToken = new CancellationTokenSource();
+            var memoryTask = Task.Run(
+                () =>
+                {
+                    while (true)
+                    {
+                        // ReSharper disable once AccessToDisposedClosure
+                        var peakWorkingSetSize = process.PeakWorkingSet64;
+
+                        result.MemoryUsed = Math.Max(result.MemoryUsed, peakWorkingSetSize);
+
+                        if (memoryTaskCancellationToken.IsCancellationRequested)
+                        {
+                            return;
+                        }
+
+                        Thread.Sleep(TimeIntervalBetweenTwoMemoryConsumptionRequests);
+                    }
+                },
+                memoryTaskCancellationToken.Token);
+
+            // Start the process
+            process.Start();
+
+            // Wait the process to complete. Kill it after (timeLimit * 1.5) milliseconds if not completed.
+            // We are waiting the process for more than defined time and after this we compare the process time with the real time limit.
+            var exited = process.WaitForExit((int)(timeLimit * 1.5));
+            if (!exited)
+            {
+                process.Kill();
+                result.Type = ProcessExecutionResultType.TimeLimit;
+            }
+
+            // Close the memory consumption check thread
+            memoryTaskCancellationToken.Cancel();
+            try
+            {
+                // To be sure that memory consumption will be evaluated correctly
+                memoryTask.Wait(TimeIntervalBetweenTwoMemoryConsumptionRequests);
+            }
+            catch (AggregateException ex)
+            {
+                logger.Warn("AggregateException caught.", ex.InnerException);
+            }
+
+            // Close the task that gets the process error output
+            try
+            {
+                errorOutputTask.Wait(100);
+            }
+            catch (AggregateException ex)
+            {
+                logger.Warn("AggregateException caught.", ex.InnerException);
+            }
+
+            // Close the task that gets the process output
+            try
+            {
+                processOutputTask.Wait(100);
+            }
+            catch (AggregateException ex)
+            {
+                logger.Warn("AggregateException caught.", ex.InnerException);
+            }
+
+            Debug.Assert(process.HasExited, "Restricted process didn't exit!");
+
+            // Report exit code and total process working time
+            result.ExitCode = process.ExitCode;
+            result.TimeWorked = process.ExitTime - process.StartTime;
+            result.PrivilegedProcessorTime = process.PrivilegedProcessorTime;
+            result.UserProcessorTime = process.UserProcessorTime;
+
+            if (result.TotalProcessorTime.TotalMilliseconds > timeLimit)
+            {
+                result.Type = ProcessExecutionResultType.TimeLimit;
+            }
+
+            if (!string.IsNullOrEmpty(result.ErrorOutput))
+            {
+                result.Type = ProcessExecutionResultType.RunTimeError;
+            }
+
+            if (result.MemoryUsed > memoryLimit)
+            {
+                result.Type = ProcessExecutionResultType.MemoryLimit;
+            }
+
+            return result;
+        }
+    }
+}
