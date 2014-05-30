4f3b2fe5e12f70d92b6b37740abc58b3930dea4a
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2014-05-30 15:26:39 +0300

    Added missing JavaPreprocessCompileArchiveExecuteAndCheckExecutionStrategy.cs and ExtendedProcess.cs files.

---
+++ Open Judge System/Workers/OJS.Workers.ExecutionStrategies/JavaPreprocessCompileArchiveExecuteAndCheckExecutionStrategy.cs
@@ -0,0 +1,237 @@
+﻿namespace OJS.Workers.ExecutionStrategies
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    using OJS.Common.Extensions;
+    using OJS.Common.Models;
+    using OJS.Workers.Checkers;
+    using OJS.Workers.Common;
+    using OJS.Workers.Executors;
+
+    public class JavaPreprocessCompileArchiveExecuteAndCheckExecutionStrategy : ExecutionStrategy
+    {
+        private const string JavaSourceFilesPattern = "*.java";
+        private const string ArchivedFileName = "_submission.jar";
+        private const string PackageNameRegEx = @"\bpackage\s+[a-zA-Z_][a-zA-Z_.0-9]{0,150}\s*;";
+        private const string ClassNameRegEx = @"public\s+class\s+([a-zA-Z_][a-zA-Z_0-9]{0,50})\s*{";
+        private const string TimeMeasurementFileName = "_time.txt";
+
+        private readonly string javaExecutablePath;
+        private readonly string javaArchiverPath;
+        private readonly string sandboxExecutorSourceFilePath;
+        private readonly string workingDirectory;
+        private readonly Func<CompilerType, string> getCompilerPathFunc;
+
+        public JavaPreprocessCompileArchiveExecuteAndCheckExecutionStrategy(
+            string javaExecutablePath,
+            string javaArchiverPath,
+            string sandboxExecutorSourceFilePath,
+            Func<CompilerType, string> getCompilerPathFunc)
+        {
+            if (!File.Exists(javaExecutablePath))
+            {
+                throw new ArgumentException(
+                    string.Format("Java not found in: {0}!", javaExecutablePath),
+                    "javaExecutablePath");
+            }
+
+            if (!File.Exists(javaArchiverPath))
+            {
+                throw new ArgumentException(
+                    string.Format("Java archiver not found in: {0}!", javaArchiverPath),
+                    "javaArchiverPath");
+            }
+
+            if (!File.Exists(sandboxExecutorSourceFilePath))
+            {
+                throw new ArgumentException(
+                    string.Format("Sandbox executor source file not found in: {0}!", sandboxExecutorSourceFilePath),
+                    "sandboxExecutorSourceFilePath");
+            }
+
+            this.javaExecutablePath = javaExecutablePath;
+            this.javaArchiverPath = javaArchiverPath;
+            this.sandboxExecutorSourceFilePath = sandboxExecutorSourceFilePath;
+            this.workingDirectory = FileHelpers.CreateTempDirectory();
+            this.getCompilerPathFunc = getCompilerPathFunc;
+        }
+
+        ~JavaPreprocessCompileArchiveExecuteAndCheckExecutionStrategy()
+        {
+            if (Directory.Exists(this.workingDirectory))
+            {
+                Directory.Delete(this.workingDirectory, true);
+            }
+        }
+
+        public override ExecutionResult Execute(ExecutionContext executionContext)
+        {
+            var result = new ExecutionResult();
+
+            // Create a temp file with the submission code
+            string submissionFilePath;
+            try
+            {
+                submissionFilePath = this.CreateSubmissionFile(executionContext.Code);
+            }
+            catch (ArgumentException exception)
+            {
+                result.IsCompiledSuccessfully = false;
+                result.CompilerComment = exception.Message;
+
+                return result;
+            }
+
+            // Copy the sandbox executor source file to the working directory
+            var sandboxExecutorSourceFilePathLastIndexOfSlash = this.sandboxExecutorSourceFilePath.LastIndexOf('\\');
+            var sandboxExecutorFileName = this.sandboxExecutorSourceFilePath.Substring(sandboxExecutorSourceFilePathLastIndexOfSlash + 1);
+
+            var tempSandboxExecutorSourceFilePath = string.Format("{0}\\{1}", this.workingDirectory, sandboxExecutorFileName);
+            File.Copy(this.sandboxExecutorSourceFilePath, tempSandboxExecutorSourceFilePath);
+
+            // Compile all source files (sandbox executor and submission file)
+            var compilerPath = this.getCompilerPathFunc(executionContext.CompilerType);
+            var sourceFilePathsToCompile = new DirectoryInfo(this.workingDirectory).GetFiles(JavaSourceFilesPattern).Select(x => x.FullName);
+            var compilerResult = this.CompileAllSourceFiles(
+                executionContext.CompilerType,
+                compilerPath,
+                executionContext.AdditionalCompilerArguments,
+                sourceFilePathsToCompile);
+
+            // Assign compiled result info to the execution result
+            result.IsCompiledSuccessfully = compilerResult.IsCompiledSuccessfully;
+            result.CompilerComment = compilerResult.CompilerComment;
+            if (!result.IsCompiledSuccessfully)
+            {
+                return result;
+            }
+
+            // Archive all compiled into a jar file
+            try
+            {
+                this.ArchiveCompiledFilesIntoJarFile();
+            }
+            catch (Exception exception)
+            {
+                result.IsCompiledSuccessfully = false;
+                result.CompilerComment = exception.Message;
+
+                return result;
+            }
+
+            // Prepare execution process arguments and time measurement info
+            var classPathWithJarFile = string.Format("-classpath \"{0}\\{1}\"", this.workingDirectory, ArchivedFileName);
+
+            var sandboxExecutorFileNameLastIndexOfDot = sandboxExecutorFileName.LastIndexOf('.');
+            var sandboxExecutorClassName = sandboxExecutorFileName.Substring(0, sandboxExecutorFileNameLastIndexOfDot);
+
+            var submissionFilePathLastIndexOfSlash = submissionFilePath.LastIndexOf('\\');
+            var submissionFilePathLastIndexOfDot = submissionFilePath.LastIndexOf('.');
+            var classToExecute = submissionFilePath.Substring(
+                submissionFilePathLastIndexOfSlash + 1,
+                submissionFilePathLastIndexOfDot - submissionFilePathLastIndexOfSlash - 1);
+
+            var timeMeasurementFilePath = string.Format("{0}\\{1}", this.workingDirectory, TimeMeasurementFileName);
+
+            // Create an executor and a checker
+            var executor = new StandardProcessExecutor();
+            var checker = Checker.CreateChecker(executionContext.CheckerAssemblyName, executionContext.CheckerTypeName, executionContext.CheckerParameter);
+
+            // Process the submission and check each test
+            foreach (var test in executionContext.Tests)
+            {
+                var processExecutionResult = executor.Execute(
+                    this.javaExecutablePath,
+                    test.Input,
+                    executionContext.TimeLimit,
+                    executionContext.MemoryLimit,
+                    new[] { classPathWithJarFile, sandboxExecutorClassName, classToExecute, string.Format("\"{0}\"", timeMeasurementFilePath) });
+
+                if (File.Exists(timeMeasurementFilePath))
+                {
+                    long timeInNanoseconds;
+                    if (long.TryParse(File.ReadAllText(timeMeasurementFilePath), out timeInNanoseconds))
+                    {
+                        processExecutionResult.TimeWorked = TimeSpan.FromMilliseconds((double)timeInNanoseconds / 1000000);
+                        File.Delete(timeMeasurementFilePath);
+                    }
+                }
+
+                var testResult = this.ExecuteAndCheckTest(test, processExecutionResult, checker, processExecutionResult.ReceivedOutput);
+                result.TestResults.Add(testResult);
+            }
+
+            return result;
+        }
+
+        private string CreateSubmissionFile(string submissionCode)
+        {
+            // Remove existing packages
+            submissionCode = Regex.Replace(submissionCode, PackageNameRegEx, string.Empty);
+
+            // TODO: Improve not to use public class
+            var classNameMatch = Regex.Match(submissionCode, ClassNameRegEx);
+            if (!classNameMatch.Success)
+            {
+                throw new ArgumentException("No valid public class found!");
+            }
+
+            var className = classNameMatch.Groups[1].Value;
+            var submissionFilePath = new FileInfo(string.Format("{0}\\{1}.java", this.workingDirectory, className)).FullName;
+            File.WriteAllText(submissionFilePath, submissionCode);
+
+            return submissionFilePath;
+        }
+
+        private CompileResult CompileAllSourceFiles(CompilerType compilerType, string compilerPath, string compilerArguments, IEnumerable<string> sourceFilePathsToCompile)
+        {
+            CompileResult compilerResult = null;
+            foreach (var sourceFilePath in sourceFilePathsToCompile)
+            {
+                compilerResult = this.Compile(compilerType, compilerPath, compilerArguments, sourceFilePath);
+
+                if (!compilerResult.IsCompiledSuccessfully)
+                {
+                    break; // The compilation of other files is not necessary
+                }
+            }
+
+            return compilerResult;
+        }
+
+        private void ArchiveCompiledFilesIntoJarFile()
+        {
+            var arguments = string.Format("cf {0} *.class", ArchivedFileName);
+
+            var processStartInfo = new ProcessStartInfo(this.javaArchiverPath)
+            {
+                Arguments = arguments,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                WorkingDirectory = this.workingDirectory
+            };
+
+            using (var process = Process.Start(processStartInfo))
+            {
+                if (process == null)
+                {
+                    throw new Exception("Could not start Java archiver!");
+                }
+
+                var errorMessage = process.StandardError.ReadToEnd();
+                if (!string.IsNullOrWhiteSpace(errorMessage))
+                {
+                    throw new Exception(errorMessage);
+                }
+
+                process.WaitForExit();
+            }
+        }
+    }
+}
---
+++ Open Judge System/Workers/OJS.Workers.Executors/Process/ExtendedProcess.cs
@@ -0,0 +1,19 @@
+﻿namespace OJS.Workers.Executors.Process
+{
+    public class ExtendedProcess : System.Diagnostics.Process
+    {
+        public ExtendedProcess()
+        {
+            this.IsDisposed = false;
+        }
+
+        public bool IsDisposed { get; private set; }
+
+        protected override void Dispose(bool disposing)
+        {
+            this.IsDisposed = disposing;
+
+            base.Dispose(disposing);
+        }
+    }
+}
