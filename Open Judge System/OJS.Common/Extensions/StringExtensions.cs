65952855a6a9b0110eadff2f250773071b5599ee
Nikolay Kostov <admin@nikolay.it>
2014-09-17 14:36:51 +0300

    Added unit tests for PascalCaseToText and ToInteger string extension methods

--- Open Judge System/OJS.Common/Extensions/StringExtensions.cs
+++ Open Judge System/OJS.Common/Extensions/StringExtensions.cs
@@ -163,5 +163,69 @@
 
             return fixedString.ToString();
         }
+
+        public static string PascalCaseToText(this string input)
+        {
+            if (input == null)
+            {
+                return null;
+            }
+
+            const char WhiteSpace = ' ';
+
+            var result = new StringBuilder();
+            var currentWord = new StringBuilder();
+            var abbreviation = new StringBuilder();
+
+            char previous = WhiteSpace;
+            bool inWord = false;
+            bool isAbbreviation = false;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char symbolToAdd = input[i];
+
+                if (Char.IsUpper(symbolToAdd) && previous == WhiteSpace && !inWord)
+                {
+                    inWord = true;
+                    isAbbreviation = true;
+                    abbreviation.Append(symbolToAdd);
+                }
+                else if (Char.IsUpper(symbolToAdd) && inWord)
+                {
+                    abbreviation.Append(symbolToAdd);
+                    currentWord.Append(WhiteSpace);
+                    symbolToAdd = Char.ToLower(symbolToAdd);
+                }
+                else if (Char.IsLower(symbolToAdd) && inWord)
+                {
+                    isAbbreviation = false;
+                }
+                else if (symbolToAdd == WhiteSpace)
+                {
+                    result.Append(isAbbreviation && abbreviation.Length > 1 ? abbreviation.ToString() : currentWord.ToString());
+                    currentWord.Clear();
+                    abbreviation.Clear();
+
+                    if (result.Length > 0)
+                    {
+                        abbreviation.Append(WhiteSpace);
+                    }
+
+                    inWord = false;
+                    isAbbreviation = false;
+                }
+
+                previous = symbolToAdd;
+                currentWord.Append(symbolToAdd);
+            }
+
+            if (currentWord.Length > 0)
+            {
+                result.Append(isAbbreviation && abbreviation.Length > 1 ? abbreviation.ToString() : currentWord.ToString());
+            }
+
+            return result.ToString();
+        }
     }
 }
---
+++ Open Judge System/Tests/OJS.Common.Tests/StringExtensions/PascalCaseToTextTests.cs
@@ -0,0 +1,64 @@
+﻿namespace OJS.Common.Tests.StringExtensions
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using OJS.Common.Extensions;
+
+    [TestClass]
+    public class PascalCaseToTextTests
+    {
+        [TestMethod]
+        public void FewWordsStringShouldReturnProperResult()
+        {
+            const string Input = "PascalCaseExample";
+            const string Expected = "Pascal case example";
+            var result = Input.PascalCaseToText();
+            Assert.AreEqual(Expected, result);
+        }
+
+        [TestMethod]
+        public void OneWordStringShouldReturnProperResult()
+        {
+            const string Input = "Pascal";
+            const string Expected = "Pascal";
+            var result = Input.PascalCaseToText();
+            Assert.AreEqual(Expected, result);
+        }
+
+        [TestMethod]
+        public void MethodShouldNotChangeTheOtherPartsOfTheString()
+        {
+            const string Input = "  PascalCase a A OtherWord Word2 ";
+            const string Expected = "  Pascal case a A Other word Word2 ";
+            var result = Input.PascalCaseToText();
+            Assert.AreEqual(Expected, result);
+        }
+
+        [TestMethod]
+        public void AbbreviationsShouldBeKept()
+        {
+            const string Input = "Ivo knows SOLID";
+            const string Expected = "Ivo knows SOLID";
+            var result = Input.PascalCaseToText();
+            Assert.AreEqual(Expected, result);
+        }
+
+        [TestMethod]
+        public void AbbreviationsShouldBeKeptIfNoOtherWords()
+        {
+            const string Input = "SOLID";
+            const string Expected = "SOLID";
+            var result = Input.PascalCaseToText();
+            Assert.AreEqual(Expected, result);
+        }
+
+        [TestMethod]
+        public void NullStringShouldReturnNull()
+        {
+            const string Input = null;
+            const string Expected = null;
+            var result = Input.PascalCaseToText();
+            Assert.AreEqual(Expected, result);
+        }
+    }
+}
