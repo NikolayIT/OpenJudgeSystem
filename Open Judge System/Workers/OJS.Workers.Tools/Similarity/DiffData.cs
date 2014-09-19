1638968657068ddd6c63da3cc53cda0500bf9c14
NikolayIT <admin@nikolay.it>
2014-09-19 15:05:51 +0300

    Integrated similarity comparison (source: http://www.mathertel.de/Diff/)

---
+++ Open Judge System/Workers/OJS.Workers.Tools/Similarity/DiffData.cs
@@ -0,0 +1,36 @@
+﻿namespace OJS.Workers.Tools.Similarity
+{
+    /// <summary>
+    /// Data on one input file being compared.  
+    /// </summary>
+    internal class DiffData
+    {
+        /// <summary>
+        /// Initialize the Diff-Data buffer.
+        /// </summary>
+        /// <param name="initData">reference to the buffer</param>
+        internal DiffData(int[] initData)
+        {
+            this.Data = initData;
+            this.Length = initData.Length;
+            this.Modified = new bool[this.Length + 2];
+        }
+
+        /// <summary>
+        /// Number of elements (lines).
+        /// </summary>
+        internal int Length { get; set; }
+
+        /// <summary>
+        /// Buffer of numbers that will be compared.
+        /// </summary>
+        internal int[] Data { get; set; }
+
+        /// <summary>
+        /// Array of booleans that flag for modified data.
+        /// This is the result of the diff.
+        /// This means deletedA in the first Data or inserted in the second Data.
+        /// </summary>
+        internal bool[] Modified { get; set; }
+    }
+}
---
+++ Open Judge System/Workers/OJS.Workers.Tools/Similarity/ShortestMiddleSnakeReturnData.cs
@@ -0,0 +1,12 @@
+﻿namespace OJS.Workers.Tools.Similarity
+{
+    /// <summary>
+    /// Shortest Middle Snake Return Data
+    /// </summary>
+    internal struct ShortestMiddleSnakeReturnData
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+    }
+}
