39a84ce2ffeeb6d417731c8d550f5cf9293fcbd2
ivaylokenov <ivaylo.kenov@gmail.com>
2014-09-19 16:46:48 +0300

    AntiCheat action for submissions similarity implemented

---
+++ Open Judge System/OJS.Common/ExpressionBuilder.cs
@@ -0,0 +1,39 @@
+﻿namespace OJS.Common
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    public static class ExpressionBuilder
+    {
+        public static Expression<Func<TElement, bool>> BuildOrExpression<TElement, TValue>(
+            IEnumerable<TValue> values,
+            Expression<Func<TElement, TValue>> valueSelector)
+        {
+            if (null == valueSelector)
+                throw new ArgumentNullException("valueSelector");
+            if (null == values)
+                throw new ArgumentNullException("values");
+            ParameterExpression p = valueSelector.Parameters.Single();
+
+            if (!values.Any())
+            {
+                return e => false;
+            }
+
+            var equals = values.Select(value =>
+                (Expression)Expression.Equal(
+                     valueSelector.Body,
+                     Expression.Constant(
+                         value,
+                         typeof(TValue)
+                     )
+                )
+            );
+            var body = equals.Aggregate<Expression>((accumulate, equal) => Expression.Or(accumulate, equal));
+
+            return Expression.Lambda<Func<TElement, bool>>(body, p);
+        }
+    }
+}
--- Open Judge System/Web/OJS.Web/Areas/Administration/Controllers/AntiCheatController.cs
+++ Open Judge System/Web/OJS.Web/Areas/Administration/Controllers/AntiCheatController.cs
@@ -1,10 +1,14 @@
 ﻿namespace OJS.Web.Areas.Administration.Controllers
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Web.Mvc;
 
+    using OJS.Common;
+    using OJS.Common.Extensions;
     using OJS.Data;
+    using OJS.Data.Models;
     using OJS.Web.Areas.Administration.ViewModels.AntiCheat;
     using OJS.Web.Controllers;
 
@@ -17,24 +21,7 @@
 
         public ActionResult ByIp()
         {
-            var contests = this.Data.Contests
-                .All()
-                .OrderByDescending(c => c.CreatedOn)
-                .Select(c =>
-                    new
-                    {
-                        Text = c.Name,
-                        Value = c.Id
-                    })
-                .ToList()
-                .Select(c =>
-                    new SelectListItem
-                    {
-                        Text = c.Text,
-                        Value = c.Value.ToString()
-                    });
-
-            return this.View(contests);
+            return this.View(this.GetContestsListItems());
         }
 
         public ActionResult RenderByIpGrid(int id, string excludeIps)
@@ -66,6 +53,107 @@
             }
 
             return this.PartialView("_IpGrid", participantsByIps);
+        }
+
+        public ActionResult BySubmissionSimilarity()
+        {
+            return this.View(this.GetContestsListItems());
+        }
+
+        public ActionResult RenderSubmissionsSimilaritiesGrid(int[] ids)
+        {
+            var orExpressionIds = ExpressionBuilder.BuildOrExpression<Submission, int>(ids, s => s.Id);
+            var participantsSimilarSubmissionGroups = this.Data.Submissions
+                .All()
+                .Where(orExpressionIds)
+                .Where(s => s.Participant.IsOfficial && s.Points != 0)
+                .Select(s => 
+                    new 
+                    {
+                        s.Id,
+                        s.ProblemId,
+                        s.ParticipantId,
+                        s.Points,
+                        s.Content,
+                        s.CreatedOn,
+                        ParticipantName = s.Participant.User.UserName,
+                        ProblemName = s.Problem.Name
+                    })
+                .GroupBy(s => new { s.ProblemId, s.ParticipantId })
+                .Select(g => g.OrderByDescending(s => s.Points).ThenByDescending(s => s.CreatedOn).FirstOrDefault())
+                .GroupBy(s => new { s.ProblemId, s.Points })
+                .ToList();
+
+            var similarities = new List<SubmissionSimilarityViewModel>();
+            foreach (var groupOfSubmissions in participantsSimilarSubmissionGroups)
+            {
+                var groupAsList = groupOfSubmissions.ToList();
+                for (int i = 0; i < groupAsList.Count; i++)
+                {
+                    for (int j = i + 1; j < groupAsList.Count; j++)
+                    {
+                        var result = this.plagiarismDetector.Check(groupAsList[i].Content.Decompress(), groupAsList[j].Content.Decompress());
+                        similarities.Add(new SubmissionSimilarityViewModel
+                        {
+                            ProblemId = groupAsList[i].ProblemId.Value,
+                            ProblemName = groupAsList[i].ProblemName,
+                            Points = groupAsList[i].Points,
+                            Differences = result.Count(),
+                            FirstSubmissionId = groupAsList[i].Id,
+                            FirstParticipantName = groupAsList[i].ParticipantName,
+                            FirstSubmissionCreatedOn = groupAsList[i].CreatedOn,
+                            SecondSubmissionId = groupAsList[j].Id,
+                            SecondParticipantName = groupAsList[j].ParticipantName,
+                            SecondSubmissionCreatedOn = groupAsList[j].CreatedOn
+                        });
+                    }
+                }
+            }
+
+            return this.PartialView("_SubmissionsGrid", similarities.GroupBy(g => g.ProblemId));
+        }
+
+        private PlagiarismDetector plagiarismDetector = new PlagiarismDetector();
+
+        private class PlagiarismResult
+        {
+            public int StartFirst { get; set; }
+
+            public int StartSecond { get; set; }
+
+            public int DeletedFirst { get; set; }
+
+            public int InsertedSecond { get; set; }
+        }
+
+        private class PlagiarismDetector
+        {
+            public IEnumerable<PlagiarismResult> Check(string first, string second)
+            {
+                return new List<PlagiarismResult>();
+            }
+        }
+
+        private IEnumerable<SelectListItem> GetContestsListItems()
+        {
+            var contests = this.Data.Contests
+                .All()
+                .OrderByDescending(c => c.CreatedOn)
+                .Select(c =>
+                    new
+                    {
+                        Text = c.Name,
+                        Value = c.Id
+                    })
+                .ToList()
+                .Select(c =>
+                    new SelectListItem
+                    {
+                        Text = c.Text,
+                        Value = c.Value.ToString()
+                    });
+
+            return contests;
         }
     }
 }
