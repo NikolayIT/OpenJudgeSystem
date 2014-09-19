513f356aa83ed7cf9500399931954994419e5795
NikolayIT <admin@nikolay.it>
2014-09-19 14:50:41 +0300

    Added two interfaces for the plagiarism detector

---
+++ Open Judge System/Workers/OJS.Workers.Tools/AntiCheat/IPlagiarismResult.cs
@@ -0,0 +1,11 @@
+﻿namespace OJS.Workers.Tools.AntiCheat
+{
+    using System.Collections.Generic;
+
+    public interface IPlagiarismResult
+    {
+        byte SimilarityPercentage { get; set; }
+
+        IEnumerable<IPlagiarismResult> Type { get; set; }
+    }
+}
