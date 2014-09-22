e77fbc25fffb06745dd664039fa298cb9394689d
NikolayIT <admin@nikolay.it>
2014-09-22 14:44:05 +0300

    Introduced settings in the OJS.Web project containing C# Compiler and ILDASM paths

---
+++ Open Judge System/Web/OJS.Web/App_Start/Settings.cs
@@ -0,0 +1,34 @@
+﻿namespace OJS.Web
+{
+    using System;
+    using System.Configuration;
+
+    public static class Settings
+    {
+        public static string CSharpCompilerPath
+        {
+            get
+            {
+                return GetSetting("CSharpCompilerPath");
+            }
+        }
+
+        public static string DotNetDisassemblerPath
+        {
+            get
+            {
+                return GetSetting("DotNetDisassemblerPath");
+            }
+        }
+
+        private static string GetSetting(string settingName)
+        {
+            if (ConfigurationManager.AppSettings[settingName] == null)
+            {
+                throw new Exception(string.Format("{0} setting not found in App.config file!", settingName));
+            }
+
+            return ConfigurationManager.AppSettings[settingName];
+        }
+    }
+}
