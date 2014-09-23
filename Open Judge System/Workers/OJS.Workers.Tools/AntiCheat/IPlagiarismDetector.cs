cc254fb1f541b39439ebadf368e145b2f356a739
NikolayIT <admin@nikolay.it>
2014-09-23 17:26:58 +0300

    Implemented visitor pattern (to change the source before comparison) in plagiarism detectors

--- Open Judge System/Workers/OJS.Workers.Tools/AntiCheat/IPlagiarismDetector.cs
+++ Open Judge System/Workers/OJS.Workers.Tools/AntiCheat/IPlagiarismDetector.cs
@@ -1,7 +1,9 @@
 ﻿namespace OJS.Workers.Tools.AntiCheat
 {
+    using System.Collections.Generic;
+
     public interface IPlagiarismDetector
     {
-        PlagiarismResult DetectPlagiarism(string firstSource, string secondSource);
+        PlagiarismResult DetectPlagiarism(string firstSource, string secondSource, IEnumerable<IDetectPlagiarismVisitor> visitors = null);
     }
 }
