cba8b7f307ce2c3685625d948ab2472c012646cb
ivaylokenov <ivaylo.kenov@gmail.com>
2015-06-16 15:45:31 +0300

    Transfered logic from Json parsing to MochaExecutionResult

---
+++ Open Judge System/Workers/OJS.Workers.ExecutionStrategies/MochaExecutionResult.cs
@@ -0,0 +1,46 @@
+﻿namespace OJS.Workers.ExecutionStrategies
+{
+    using Newtonsoft.Json.Linq;
+
+    public class MochaExecutionResult
+    {
+        public static MochaExecutionResult Parse(string result)
+        {
+            JObject jsonTestResult = null;
+            var passed = false;
+            string error = null;
+
+            try
+            {
+                jsonTestResult = JObject.Parse(result.Trim());
+                passed = (int)jsonTestResult["stats"]["passes"] == 1;
+            }
+            catch
+            {
+                error = "Invalid console output!";
+            }
+
+            if (!passed)
+            {
+                try
+                {
+                    error = (string)jsonTestResult["failures"][0]["err"]["message"];
+                }
+                catch
+                {
+                    error = "Invalid console output!";
+                }
+            }
+
+            return new MochaExecutionResult
+            {
+                Passed = passed,
+                Error = error
+            };
+        }
+
+        public bool Passed { get; set; }
+
+        public string Error { get; set; }
+    }
+}
