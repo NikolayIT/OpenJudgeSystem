c6f8cf323843dd5f1a7dec05d91b98416af223f4
ivaylokenov <ivaylo.kenov@gmail.com>
2015-07-22 10:42:14 +0300

    Add NodeJsPreprocessExecuteAndRunJsDomUnitTestsExecutionStrategy inheriting from Node Mocha Execution strategy

---
+++ Open Judge System/Workers/OJS.Workers.ExecutionStrategies/NodeJsPreprocessExecuteAndRunJsDomUnitTestsExecutionStrategy.cs
@@ -0,0 +1,33 @@
+﻿namespace OJS.Workers.ExecutionStrategies
+{
+    using System;
+    using System.IO;
+
+    public class NodeJsPreprocessExecuteAndRunJsDomUnitTestsExecutionStrategy : NodeJsPreprocessExecuteAndRunUnitTestsWithMochaExecutionStrategy
+    {
+        private string jsDomModulePath;
+        private string jQueryModulePath;
+
+        public NodeJsPreprocessExecuteAndRunJsDomUnitTestsExecutionStrategy(
+            string nodeJsExecutablePath,
+            string mochaModulePath, 
+            string chaiModulePath, 
+            string jsDomModulePath, 
+            string jQueryModulePath)
+            : base(nodeJsExecutablePath, mochaModulePath, chaiModulePath)
+        {
+            if (!File.Exists(jsDomModulePath))
+            {
+                throw new ArgumentException(string.Format("jsDom not found in: {0}", jsDomModulePath), "jsDomModulePath");
+            }
+
+            if (!File.Exists(jQueryModulePath))
+            {
+                throw new ArgumentException(string.Format("jQuery not found in: {0}", jQueryModulePath), "jQueryModulePath");
+            }
+
+            this.jsDomModulePath = jsDomModulePath;
+            this.jQueryModulePath = jQueryModulePath;
+        }
+    }
+}
