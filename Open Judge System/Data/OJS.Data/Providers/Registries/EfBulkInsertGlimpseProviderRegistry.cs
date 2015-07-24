a1c972095acfd2311b3d75b0732e3d83106a8027
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2015-07-24 18:53:39 +0300

    Implemented GlimpseConnectionEfSqlBulkInsertProvider and EfBulkInsertGlimpseProviderRegistry to fix incompatibility problem between Glimpse and EfBulkInsert libraries.

---
+++ Open Judge System/Data/OJS.Data/Providers/Registries/EfBulkInsertGlimpseProviderRegistry.cs
@@ -0,0 +1,12 @@
+﻿namespace OJS.Data.Providers.Registries
+{
+    using EntityFramework.BulkInsert;
+
+    public static class EfBulkInsertGlimpseProviderRegistry
+    {
+        public static void Execute()
+        {
+            ProviderFactory.Register<GlimpseConnectionEfSqlBulkInsertProvider>("Glimpse.Ado.AlternateType.GlimpseDbConnection");
+        }
+    }
+}
