74d1e001ec5fddb739cd50aee13cf9d033727d59
Teodor92 <teodor.ivanov92@gmail.com>
2015-08-07 02:53:52 +0300

    Added enum extensions and a new Attribute for easy localization

---
+++ Open Judge System/OJS.Common/Attributes/LocalizedDescriptionAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace OJS.Common.Attributes
+{
+    using System;
+    using System.ComponentModel;
+    using System.Reflection;
+
+    public class LocalizedDescriptionAttribute : DescriptionAttribute
+    {
+        public LocalizedDescriptionAttribute(string resourceName, Type resourceType = null)
+        {
+            if (resourceType != null)
+            {
+                var resourceProperty = resourceType.GetProperty(resourceName, BindingFlags.Static | BindingFlags.Public);
+                if (resourceProperty != null)
+                {
+                    this.DescriptionValue = (string)resourceProperty.GetValue(resourceProperty.DeclaringType, null);
+                }
+            }
+        }
+    }
+}
