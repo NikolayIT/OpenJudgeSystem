1a8dd4f43906cdaa17078934870b89646d8b9da7
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2016-02-09 09:16:41 +0200

    Removed unused DirectoryHelpers.Copy method.

--- Open Judge System/OJS.Common/Extensions/DirectoryHelpers.cs
+++ Open Judge System/OJS.Common/Extensions/DirectoryHelpers.cs
@@ -1,6 +1,5 @@
 ﻿namespace OJS.Common.Extensions
 {
-    using System;
     using System.IO;
 
     public static class DirectoryHelpers
@@ -27,45 +26,6 @@
                 Directory.EnumerateFileSystemEntries(path, "*", searchOption).ForEach(x => File.SetAttributes(x, FileAttributes.Normal));
 
                 Directory.Delete(path, recursive);
-            }
-        }
-
-        public static void Copy(string sourceDirectoryPath, string destinationDirectoryPath, bool overwriteFiles = false, bool recursive = true)
-        {
-            var sourceDirectory = new DirectoryInfo(sourceDirectoryPath);
-            if (!sourceDirectory.Exists)
-            {
-                throw new ArgumentException("Source directory not found on file system.", nameof(sourceDirectoryPath));
-            }
-
-            var destinationDirectoryFullPath = Path.GetFullPath(destinationDirectoryPath);
-            if (destinationDirectoryFullPath != sourceDirectory.FullName)
-            {
-                Directory.CreateDirectory(destinationDirectoryFullPath);
-
-                var files = sourceDirectory.GetFiles();
-                foreach (var file in files)
-                {
-                    var newFilePath = Path.Combine(destinationDirectoryFullPath, file.Name);
-                    if (overwriteFiles || !File.Exists(newFilePath))
-                    {
-                        File.Copy(file.FullName, newFilePath, overwriteFiles);
-                    }
-                }
-
-                var subDirectories = sourceDirectory.GetDirectories();
-                foreach (var directory in subDirectories)
-                {
-                    var newDirectoryPath = Path.Combine(destinationDirectoryFullPath, directory.Name);
-                    if (recursive)
-                    {
-                        Copy(directory.FullName, newDirectoryPath, overwriteFiles);
-                    }
-                    else
-                    {
-                        Directory.CreateDirectory(newDirectoryPath);
-                    }
-                }
             }
         }
     }
