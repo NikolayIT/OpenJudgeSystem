17d4428072e26beb5e6b3c49b163194f8d7427fd
apxoht <ivaylo.kenov@gmail.com>
2017-02-26 16:11:24 +0200

    Added ParticipantScore data model

---
+++ Open Judge System/Data/OJS.Data.Models/ParticipantScore.cs
@@ -0,0 +1,28 @@
+﻿namespace OJS.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class ParticipantScore
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ProblemId { get; set; }
+
+        public virtual Problem Problem { get; set; }
+
+        public int ParticipantId { get; set; }
+
+        public virtual Participant Participant { get; set; }
+
+        public int SubmissionId { get; set; }
+
+        public virtual Submission Submission { get; set; }
+
+        public int Result { get; set; }
+
+        [Index]
+        public bool IsOfficial { get; set; }
+    }
+}
