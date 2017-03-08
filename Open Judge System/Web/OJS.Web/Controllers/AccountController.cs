e0ea93b525fff103d4b25ae931c1ec82e2a2acba
apxoht <ivaylo.kenov@gmail.com>
2017-03-08 17:40:00 +0200

    Fixed AccountController Login action to use external SoftUni platform

--- Open Judge System/Web/OJS.Web/Controllers/AccountController.cs
+++ Open Judge System/Web/OJS.Web/Controllers/AccountController.cs
@@ -54,45 +54,6 @@
         }
 
         // POST: /Account/Login
-        //[HttpPost]
-        //[AllowAnonymous]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Login(LoginViewModel model, string returnUrl)
-        //{
-        //    if (this.ModelState.IsValid)
-        //    {
-        //        ExternalUserViewModel externalUser = null;
-        //        try
-        //        {
-        //            externalUser = await this.GetExternalUser(model.UserName);
-        //        }
-        //        catch (Exception)
-        //        {
-        //            this.TempData[GlobalConstants.InfoMessage] = "Съжаляваме, но в момента системата за вход не е активна.";
-        //            return this.RedirectToAction("Index", "Home", new { area = string.Empty });
-        //        }
-
-        //        if (externalUser != null)
-        //        {
-        //            var userEntity = externalUser.Entity;
-        //            this.AddOrUpdateUser(userEntity);
-
-        //            var user = await this.UserManager.FindAsync(model.UserName, model.Password);
-        //            if (user != null)
-        //            {
-        //                await this.SignInAsync(userEntity, model.RememberMe);
-        //                return this.RedirectToLocal(returnUrl);
-        //            }
-        //        }
-
-        //        this.ModelState.AddModelError(string.Empty, Resources.Account.AccountViewModels.Invalid_username_or_password);
-        //    }
-
-        //    // If we got this far, something failed, redisplay form
-        //    return this.View(model);
-        //}
-
-        // POST: /Account/Login
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
@@ -100,11 +61,28 @@
         {
             if (this.ModelState.IsValid)
             {
-                var user = await this.UserManager.FindAsync(model.UserName, model.Password);
-                if (user != null)
+                ExternalUserViewModel externalUser = null;
+                try
                 {
-                    await this.SignInAsync(user, model.RememberMe);
-                    return this.RedirectToLocal(returnUrl);
+                    externalUser = await this.GetExternalUser(model.UserName);
+                }
+                catch (Exception)
+                {
+                    this.TempData[GlobalConstants.InfoMessage] = "Съжаляваме, но в момента системата за вход не е активна.";
+                    return this.RedirectToAction("Index", "Home", new { area = string.Empty });
+                }
+
+                if (externalUser != null)
+                {
+                    var userEntity = externalUser.Entity;
+                    this.AddOrUpdateUser(userEntity);
+
+                    var user = await this.UserManager.FindAsync(model.UserName, model.Password);
+                    if (user != null)
+                    {
+                        await this.SignInAsync(userEntity, model.RememberMe);
+                        return this.RedirectToLocal(returnUrl);
+                    }
                 }
 
                 this.ModelState.AddModelError(string.Empty, Resources.Account.AccountViewModels.Invalid_username_or_password);
