689c6a5384161100de500b7913875274b333c3c6
Svetlin Galov <svetlin.galov@gmail.com>
2017-05-03 15:20:45 +0300

    CR: refactored whitespace SoftUni-Internal/suls-issues#2055

--- Open Judge System/Data/OJS.Data/Migrations/DefaultMigrationConfiguration.cs
+++ Open Judge System/Data/OJS.Data/Migrations/DefaultMigrationConfiguration.cs
@@ -20,7 +20,7 @@
         }
 
         protected override void Seed(OjsDbContext context)
-        {           
+        {        
             if (context.Roles.Any())
             {
                 return;
