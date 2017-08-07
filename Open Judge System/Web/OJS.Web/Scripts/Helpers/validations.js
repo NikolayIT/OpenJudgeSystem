c3e3a5de5a86de767c2888343a35788d1d078fe8
Teodor92 <teodor.ivanov92@gmail.com>
2017-08-07 14:00:02 +0300

    Applied eslint rules to validations.js

--- Open Judge System/Web/OJS.Web/Scripts/Helpers/validations.js
+++ Open Judge System/Web/OJS.Web/Scripts/Helpers/validations.js
@@ -1,10 +1,14 @@
-﻿function addSubmitValidation(form, elements, validartionField, validationFunction, validationMessage) {
-    form.on("submit", function (ev) {
+﻿// TODO: Add to module
+
+function addSubmitValidation(form, elements, validartionField, validationFunction, validationMessage) {
+    'use strict';
+
+    form.on('submit', function (ev) {
         if (validationFunction(elements)) {
-            validartionField.attr("class", "field-validation-valid");
-            validartionField.text("");
+            validartionField.attr('class', 'field-validation-valid');
+            validartionField.text('');
         } else {
-            validartionField.attr("class", "field-validation-error");
+            validartionField.attr('class', 'field-validation-error');
             validartionField.text(validationMessage);
             ev.preventDefault();
         }
@@ -12,20 +16,24 @@
 }
 
 function reparseForm(form) {
-    form.removeData("validator");
-    form.removeData("unobtrusiveValidation");
+    'use strict';
+
+    form.removeData('validator');
+    form.removeData('unobtrusiveValidation');
     $.validator.unobtrusive.parse(form);
 }
 
 function validateZipFile(files, validationField, form) {
+    'use strict';
+
     if (files) {
-        if (files[0].extension.toLowerCase() !== ".zip") {
-            validationField.attr("class", "field-validation-error");
-            validationField.text("Uploaded file must be in .zip format");
-            e.preventDefault();
+        if (files[0].extension.toLowerCase() === '.zip') {
+            validationField.attr('class', 'field-validation-valid');
+            validationField.text('');
         } else {
-            validationField.attr("class", "field-validation-valid");
-            validationField.text("");
+            validationField.attr('class', 'field-validation-error');
+            validationField.text('Uploaded file must be in .zip format');
+            e.preventDefault();
         }
     }
 
