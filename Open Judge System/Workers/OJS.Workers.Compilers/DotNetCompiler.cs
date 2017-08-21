a18e4abf0234e76cb4467d4b06fdee7e647a8f45
Svetlin Galov <svetlin.galov@gmail.com>
2017-08-21 16:32:56 +0300

    Implemented DotNetCoreProjectExecutionStrategy for testing .NET Core console application projects. Projects are compiled and executed via the DotNetCLI. Also extracted some logic for future execution strategies that will revolve around unit testing. closes https://github.com/SoftUni-Internal/suls-issues/issues/2856

--- Open Judge System/Workers/OJS.Workers.Compilers/DotNetCompiler.cs
+++ Open Judge System/Workers/OJS.Workers.Compilers/DotNetCompiler.cs
@@ -1,19 +1,38 @@
 ﻿namespace OJS.Workers.Compilers
 {
+    using System.IO;
+    using System.Linq;
     using System.Text;
+
     using OJS.Common;
+    using OJS.Common.Extensions;
 
     public class DotNetCompiler : Compiler
     {
-        public override int MaxProcessExitTimeOutMillisecond => GlobalConstants.DefaultProcessExitTimeOutMilliseconds +
-                                                                5000;
-
+        public override int MaxProcessExitTimeOutMillisecond => GlobalConstants.DefaultProcessExitTimeOutMilliseconds * 2;
+        
         public override bool ShouldDeleteSourceFile => false;
+
+        public override string ChangeOutputFileAfterCompilation(string outputFile)
+        {
+            string compiledFileName = Path.GetFileNameWithoutExtension(Path.GetFileNameWithoutExtension(outputFile));
+            string rootDir = Path.GetDirectoryName(outputFile);
+            string compiledFile =
+                FileHelpers.FindFileMatchingPattern(
+                    rootDir,
+                    $"{compiledFileName}{GlobalConstants.ClassLibraryFileExtension}");
+
+            return compiledFile;
+        }
 
         public override string BuildCompilerArguments(string inputFile, string outputFile, string additionalArguments)
         {
+            var compilingDir = $"{Path.GetDirectoryName(inputFile)}\\CompileDir";
+            Directory.CreateDirectory(compilingDir);
+
             var arguments = new StringBuilder();
             arguments.Append("build ");
+            arguments.Append($"-o {compilingDir} ");
             arguments.Append($"\"{inputFile}\" ");
             arguments.Append(additionalArguments);
             return arguments.ToString().Trim();
--- Open Judge System/Workers/OJS.Workers.ExecutionStrategies/DotNetCoreProjectExecutionStrategy.cs
+++ Open Judge System/Workers/OJS.Workers.ExecutionStrategies/DotNetCoreProjectExecutionStrategy.cs
@@ -2,37 +2,16 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.IO;
-    using System.Text.RegularExpressions;
-    using System.Xml;
-    using Checkers;
-    using Common;
-    using Executors;
-    using OJS.Common;
+
     using OJS.Common.Extensions;
     using OJS.Common.Models;
+    using OJS.Workers.Checkers;
+    using OJS.Workers.Executors;
 
     public class DotNetCoreProjectExecutionStrategy : CSharpProjectTestsExecutionStrategy
     {
-        protected const string GenerateProgramFileNodeName = @"GenerateProgramFile";
-        protected const string PropertyGroupNodeName = @"PropertyGroup";
-        protected const string PackageReferenceNodeName = @"PackageReference";
-
-        protected const string ItemGroupNodeXPath = @"ItemGroup";
-        protected const string GenerateProgramFileXPath = @"PropertyGroup/GenerateProgramFile[.='false' or .='False']";
-        protected const string PackageReferenceXPathExpressionTemplate =
-            @"ItemGroup/PackageReference[@Include='##packageName##']";
-
-        protected const string SuccessfulRestorationPattern = @"Restore completed in \d+[,.]*\d+ sec for (.+)##csProjFileName##\.csproj";
-
-        // TODO: update to newer versions
-        protected static readonly Dictionary<string, string> RequiredReferencedAndVersions = new Dictionary<string, string>
-        {
-            { "Microsoft.NET.Test.Sdk", "15.5.0-preview-20170727-01"},
-            { "NUnit", "3.7.1" },
-            { "NUnit3TestAdapter", "3.8" }
-        };
+        protected new const string AdditionalExecutionArguments = "--no-build --no-restore";
 
         public DotNetCoreProjectExecutionStrategy(Func<CompilerType, string> getCompilerPathFunc)
             : base(getCompilerPathFunc)
@@ -57,37 +36,7 @@
                 CsProjFileSearchPattern,
                 f => new FileInfo(f).Length);
 
-            this.ExtractTestNames(executionContext.Tests);
-            var compileDirectory = Path.GetDirectoryName(csProjFilePath);
-            this.SetupFixturePath = $"{compileDirectory}\\{SetupFixtureFileName}{GlobalConstants.CSharpFileExtension}";
-
-            this.PrepareCsProjFile(csProjFilePath);
-
-            var compilerPath = this.GetCompilerPathFunc(executionContext.CompilerType);
-
-            List<string> arguments = new List<string>();
-            arguments.Add("restore");
-            arguments.Add(csProjFilePath);
-
-            var restoreResult = this.RunDotNetRestoreOnProject(
-                compilerPath,
-                executionContext.TimeLimit,
-                executionContext.MemoryLimit,
-                arguments);
-
-            var restoreMatcher = new Regex(SuccessfulRestorationPattern.Replace(
-                "##csProjFileName##",
-                Path.GetFileNameWithoutExtension(csProjFilePath)));
-
-            if (restoreResult.Type != ProcessExecutionResultType.Success || 
-                !restoreMatcher.IsMatch(restoreResult.ReceivedOutput))
-            {
-                result.IsCompiledSuccessfully = false;
-                return result;
-            }
-
-            //var expendableCsProjFile = $"{Path.GetDirectoryName(csProjFilePath)}\\{Path.GetFileNameWithoutExtension(csProjFilePath)}Copy.csproj";
-            //File.Copy(csProjFilePath, expendableCsProjFile);
+            var compilerPath = this.GetCompilerPathFunc(executionContext.CompilerType);         
 
             var compilerResult = this.Compile(
                 executionContext.CompilerType,
@@ -95,142 +44,43 @@
                 executionContext.AdditionalCompilerArguments,
                 csProjFilePath);
 
-            if (!compilerResult.IsCompiledSuccessfully)
+            result.IsCompiledSuccessfully = compilerResult.IsCompiledSuccessfully;
+
+            if (!result.IsCompiledSuccessfully)
             {
-                result.IsCompiledSuccessfully = false;
                 return result;
             }
-
+           
             var executor = new RestrictedProcessExecutor();
             var checker = Checker.CreateChecker(
                 executionContext.CheckerAssemblyName,
                 executionContext.CheckerTypeName,
                 executionContext.CheckerParameter);
 
-            result = this.RunUnitTests(executionContext, executor, checker, result, csProjFilePath);
+            List<string> arguments = new List<string>();
+            arguments.Add(compilerResult.OutputFile);
+            arguments.Add(AdditionalExecutionArguments);
+
+            foreach (var test in executionContext.Tests)
+            {
+                var processExecutionResult = executor.Execute(
+                    compilerPath,
+                    test.Input,
+                    executionContext.TimeLimit,
+                    executionContext.MemoryLimit,
+                    arguments,
+                    this.WorkingDirectory);
+
+                var testResult = this.ExecuteAndCheckTest(
+                    test,
+                    processExecutionResult,
+                    checker,
+                    processExecutionResult.ReceivedOutput);
+
+                result.TestResults.Add(testResult);
+            }
 
             return result;
-        }
-
-        protected override ExecutionResult RunUnitTests(
-            ExecutionContext executionContext,
-            IExecutor executor, 
-            IChecker checker,
-            ExecutionResult result,
-            string compiledFile)
-        {
-            List<string> arguments = new List<string>();
-            arguments.Add("test");
-            arguments.Add(compiledFile);
-            arguments.Add("--no-build");
-            arguments.Add("--no-restore");
-            arguments.Add(@"-s C:\Windows\Temp\setting.runsettings");
-            arguments.Add(@"-- NUnit.InProcess=true");
-
-            var dotNetCli = this.GetCompilerPathFunc(executionContext.CompilerType);
-
-            var executionResult = executor.Execute(
-                dotNetCli,
-                string.Empty,
-                executionContext.TimeLimit,
-                executionContext.MemoryLimit,
-                arguments,
-                this.WorkingDirectory);                  
-
-            return null;
-        }
-         
-        private ProcessExecutionResult RunDotNetRestoreOnProject(
-            string dotNetCli,
-            int timeLimit,
-            int memoryLimit,
-            IEnumerable<string> arguments)
-        {
-            var processExecutor = new StandardProcessExecutor();
-            var restoreResult = processExecutor.Execute(dotNetCli, string.Empty, timeLimit * 2, memoryLimit * 2, arguments);
-            return restoreResult;
-        }
-
-        private void PrepareCsProjFile(string csProjFilePath)
-        {
-            XmlDocument document = new XmlDocument();
-            document.Load(csProjFilePath);
-
-            this.AddPropertyGroupToCsProj(document, GenerateProgramFileXPath, csProjFilePath);
-
-            foreach (var requiredReferencedAndVersion in RequiredReferencedAndVersions)
-            {
-                var packageName = requiredReferencedAndVersion.Key;
-                var packageVersion = requiredReferencedAndVersion.Value;
-                var xpathExpression =
-                    PackageReferenceXPathExpressionTemplate.Replace("##packageName##", packageName);
-
-                this.AddPackageReferenceToCsProj(
-                    document,
-                    xpathExpression,
-                    csProjFilePath,
-                    new[] { packageName, packageVersion });
-            }
-
-        }
-
-        private void AddPropertyGroupToCsProj(
-            XmlDocument document,
-            string xpathExpression,
-            string savePath)
-        {
-            XmlNode rootNode = document.DocumentElement;
-            if (this.NodeExists(rootNode, xpathExpression))
-            {
-                return;
-            }
-
-            XmlNode generateProgramFileNode = document.CreateNode(XmlNodeType.Element, GenerateProgramFileNodeName, string.Empty);
-            XmlNode propertyGroupNode = document.CreateNode(XmlNodeType.Element, PropertyGroupNodeName, string.Empty);
-
-            generateProgramFileNode.InnerText = "false";
-            propertyGroupNode.AppendChild(generateProgramFileNode);
-            rootNode.AppendChild(propertyGroupNode);
-            document.Save(savePath);
-        }
-
-        private void AddPackageReferenceToCsProj(XmlDocument document, string xpathExpression, string savePath, params string[] nodeValues)
-        {
-            XmlNode rootNode = document.DocumentElement;
-            if (this.NodeExists(rootNode, xpathExpression))
-            {
-                return;
-            }
-
-            XmlNode packageReference = document.CreateNode(XmlNodeType.Element, PackageReferenceNodeName, string.Empty);
-            XmlAttribute includeAttribute = document.CreateAttribute("Include");
-            XmlAttribute versionAttribute = document.CreateAttribute("Version");
-            includeAttribute.InnerText = nodeValues[0];
-            versionAttribute.InnerText = nodeValues[1];
-
-            packageReference.Attributes.Append(includeAttribute);
-            packageReference.Attributes.Append(versionAttribute);
-
-            var itemGroupNode = rootNode.SelectSingleNode(ItemGroupNodeXPath);
-            itemGroupNode.AppendChild(packageReference);
-            document.Save(savePath);
-        }
-
-        private bool NodeExists(XmlNode rootNode, string xpathExpression)
-        {
-            if (rootNode == null)
-            {
-                throw new ArgumentException("Project file is malformed");
-            }
-
-            var targetNode = rootNode.SelectSingleNode(xpathExpression);
-
-            if (targetNode != null)
-            {
-                return true;
-            }
-
-            return false;
         }
     }
 }
