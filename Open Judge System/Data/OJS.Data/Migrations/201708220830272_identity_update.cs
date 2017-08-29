6e90e8966c89a364a2da080b4fbbace8d737533a
jicata <svetlin.galov@gmail.com>
2017-08-29 15:37:29 +0300

    CR:refactoring

--- Open Judge System/Data/OJS.Data/Migrations/201708220830272_identity_update.cs
+++ Open Judge System/Data/OJS.Data/Migrations/201708220830272_identity_update.cs
@@ -7,26 +7,26 @@ namespace OJS.Data.Migrations
     {
         public override void Up()
         {
-            RenameColumn(table: "dbo.AspNetUserClaims", name: "User_Id", newName: "UserId");
-            RenameIndex(table: "dbo.AspNetUserClaims", name: "IX_User_Id", newName: "IX_UserId");
-            DropPrimaryKey("dbo.AspNetUserLogins");
-            AddColumn("dbo.AspNetUsers", "EmailConfirmed", c => c.Boolean(nullable: false));
-            AddColumn("dbo.AspNetUsers", "PhoneNumber", c => c.String());
-            AddColumn("dbo.AspNetUsers", "PhoneNumberConfirmed", c => c.Boolean(nullable: false));
-            AddColumn("dbo.AspNetUsers", "TwoFactorEnabled", c => c.Boolean(nullable: false));
-            AddColumn("dbo.AspNetUsers", "LockoutEndDateUtc", c => c.DateTime());
-            AddColumn("dbo.AspNetUsers", "LockoutEnabled", c => c.Boolean(nullable: false));
-            AddColumn("dbo.AspNetUsers", "AccessFailedCount", c => c.Int(nullable: false));
-            AlterColumn("dbo.AspNetUsers", "UserName", c => c.String(nullable: false, maxLength: 256, unicode: false));
-            AlterColumn("dbo.AspNetUsers", "Email", c => c.String(nullable: false, maxLength: 256, unicode: false));
-            AlterColumn("dbo.AspNetUsers", "IsGhostUser", c => c.Boolean(nullable: false));
-            AlterColumn("dbo.AspNetUsers", "IsDeleted", c => c.Boolean(nullable: false));
-            AlterColumn("dbo.AspNetUsers", "CreatedOn", c => c.DateTime(nullable: false));
-            AlterColumn("dbo.AspNetRoles", "Name", c => c.String(nullable: false, maxLength: 256));
-            AddPrimaryKey("dbo.AspNetUserLogins", new[] { "LoginProvider", "ProviderKey", "UserId" });
-            CreateIndex("dbo.AspNetUsers", "UserName", unique: true, name: "UserNameIndex");
-            CreateIndex("dbo.AspNetRoles", "Name", unique: true, name: "RoleNameIndex");
-            DropColumn("dbo.AspNetUsers", "Discriminator");
+            this.RenameColumn(table: "dbo.AspNetUserClaims", name: "User_Id", newName: "UserId");
+            this.RenameIndex(table: "dbo.AspNetUserClaims", name: "IX_User_Id", newName: "IX_UserId");
+            this.DropPrimaryKey("dbo.AspNetUserLogins");
+            this.AddColumn("dbo.AspNetUsers", "EmailConfirmed", c => c.Boolean(nullable: false));
+            this.AddColumn("dbo.AspNetUsers", "PhoneNumber", c => c.String());
+            this.AddColumn("dbo.AspNetUsers", "PhoneNumberConfirmed", c => c.Boolean(nullable: false));
+            this.AddColumn("dbo.AspNetUsers", "TwoFactorEnabled", c => c.Boolean(nullable: false));
+            this.AddColumn("dbo.AspNetUsers", "LockoutEndDateUtc", c => c.DateTime());
+            this.AddColumn("dbo.AspNetUsers", "LockoutEnabled", c => c.Boolean(nullable: false));
+            this.AddColumn("dbo.AspNetUsers", "AccessFailedCount", c => c.Int(nullable: false));
+            this.AlterColumn("dbo.AspNetUsers", "UserName", c => c.String(nullable: false, maxLength: 256, unicode: false));
+            this.AlterColumn("dbo.AspNetUsers", "Email", c => c.String(nullable: false, maxLength: 256, unicode: false));
+            this.AlterColumn("dbo.AspNetUsers", "IsGhostUser", c => c.Boolean(nullable: false));
+            this.AlterColumn("dbo.AspNetUsers", "IsDeleted", c => c.Boolean(nullable: false));
+            this.AlterColumn("dbo.AspNetUsers", "CreatedOn", c => c.DateTime(nullable: false));
+            this.AlterColumn("dbo.AspNetRoles", "Name", c => c.String(nullable: false, maxLength: 256));
+            this.AddPrimaryKey("dbo.AspNetUserLogins", new[] { "LoginProvider", "ProviderKey", "UserId" });
+            this.CreateIndex("dbo.AspNetUsers", "UserName", unique: true, name: "UserNameIndex");
+            this.CreateIndex("dbo.AspNetRoles", "Name", unique: true, name: "RoleNameIndex");
+            this.DropColumn("dbo.AspNetUsers", "Discriminator");
         }
         
         public override void Down()
