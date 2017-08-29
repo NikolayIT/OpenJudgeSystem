005f8a875c98bfa67cb167b44dee46ae2f4322a9
jicata <svetlin.galov@gmail.com>
2017-08-29 13:34:41 +0300

    .cs files left behind after last commit. ContestInstancesRepository.cs & ISubmissionsForProcessingRepository.cs

---
+++ Open Judge System/Data/OJS.Data/Repositories/Contracts/ISubmissionsForProcessingRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace OJS.Data.Repositories.Contracts
+{
+    using Data.Contracts;
+    using Models;
+
+    public interface ISubmissionsForProcessingRepository : IRepository<SubmissionsForProcessing>
+    {
+        void AddOrUpdateSubmissionForProcessing(int submissionId);
+    }
+}
---
+++ Open Judge System/Data/OJS.Data/Repositories/SubmissionsForProcessingRepository.cs
@@ -0,0 +1,37 @@
+﻿namespace OJS.Data.Repositories
+{
+    using System.Linq;
+    using Base;
+    using Contracts;
+    using Models;
+
+    public class SubmissionsForProcessingRepository : GenericRepository<SubmissionsForProcessing>, ISubmissionsForProcessingRepository
+    {
+        public SubmissionsForProcessingRepository(IOjsDbContext context) 
+            : base(context)
+        {
+        }
+
+        public void AddOrUpdateSubmissionForProcessing(int submissionId)
+        {
+            var submissionForProcessing = this.Context.SubmissionsForProcessing
+                .FirstOrDefault(sfp => sfp.SubmissionId == submissionId);
+
+            if (submissionForProcessing != null)
+            {
+                submissionForProcessing.Processing = false;
+                submissionForProcessing.Processed = false;
+            }
+            else
+            {
+                submissionForProcessing = new SubmissionsForProcessing()
+                {
+                    SubmissionId = submissionId
+                };
+                this.Context.SubmissionsForProcessing.Add(submissionForProcessing);
+            }
+
+            this.Context.SaveChanges();      
+        }
+    }
+}
