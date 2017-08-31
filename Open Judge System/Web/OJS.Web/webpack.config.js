a98962b5d70b3f407d8c4d9ef82770158fb22536
Teodor92 <teodor.ivanov92@gmail.com>
2017-08-31 15:33:13 +0300

    Started webpack config and npm build config

---
+++ Open Judge System/Web/OJS.Web/webpack.config.js
@@ -0,0 +1,37 @@
+﻿const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const Assets = [{
+    origin: "bootstrap/dist/",
+    target: "bootstrap"
+}, {
+    origin: "jquery/dist/",
+    target: "jquery"
+}, {
+    origin: "jquery-validation/dist/",
+    target: "jquery-validation"
+}, {
+    origin: "jquery-validation-unobtrusive/jquery.validate.unobtrusive.js",
+    target: "jquery-validation-unobtrusive"
+}];
+
+module.exports = {
+    entry: {
+        app: './Scripts/global.js'
+    },
+    output: {
+        path: __dirname + '/Test/',
+        filename: '[name].bundle.js'
+    },
+    plugins: [
+        new CopyWebpackPlugin(
+            Assets.map(asset => {
+                return {
+                    from: path.resolve(__dirname, `./node_modules/${asset.origin}`),
+                    to: path.resolve(__dirname, `./Test/npm/${asset.target}`)
+                };
+            })
+        )
+    ]
+};
