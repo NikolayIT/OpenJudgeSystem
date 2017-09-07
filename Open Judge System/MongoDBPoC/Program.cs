8b22c8c47acf2d2834fa126ce71206a8d5cf9ba2
jicata <svetlin.galov@gmail.com>
2017-09-07 13:39:09 +0300

    Added mongocsharpdriver to OJS.Data and created a small PoC for showcasing MongoDB workflow. closes https://github.com/SoftUni-Internal/suls-issues/issues/3007

---
+++ Open Judge System/MongoDBPoC/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace MongoDBPoC
+{
+    using System;
+    using System.Collections.Generic;
+
+    using MongoDB.Bson;
+    using MongoDB.Driver;
+
+    public class Program
+    {
+        static void Main()
+        {
+            const string ConnectionString = "mongodb://localhost:27017";
+            var client = new MongoClient(ConnectionString);
+            IMongoDatabase db = client.GetDatabase("OJS");
+            var collection = db.GetCollection<SubmissionForProcessing>("submissionsForProcessing");
+
+            InsertSubmissions(collection);
+
+            var filter = new FilterDefinitionBuilder<SubmissionForProcessing>().Where(s => s.Processed);
+            collection.Find(filter).ToList()
+                .ForEach(s => Console.WriteLine(
+                             $"ID: {s.SubmissionId}; Processed: {s.Processed}; Processing: {s.Processing}"));
+
+        }
+
+        private static void InsertSubmissions(IMongoCollection<SubmissionForProcessing> collection)
+        {
+            var submissionOne = new SubmissionForProcessing()
+            {
+                Processed = false,
+                Processing = true,
+                SubmissionId = 1
+            };
+            var submissionTwo = new SubmissionForProcessing()
+            {
+                Processed = false,
+                Processing = false,
+                SubmissionId = 2
+            };
+            var submissionThree = new SubmissionForProcessing()
+            {
+                Processed = true,
+                Processing = false,
+                SubmissionId = 3
+            };
+
+            var submissions = new List<SubmissionForProcessing>()
+            {
+                submissionOne,
+                submissionTwo,
+                submissionThree
+            };
+
+            collection.InsertMany(submissions);
+        }
+
+        private class SubmissionForProcessing
+        {
+            public ObjectId Id { get; set; }
+
+            public int SubmissionId { get; set; }
+
+            public bool Processing { get; set; }
+
+            public bool Processed { get; set; }
+        }
+    }
+}
