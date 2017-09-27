903cc72eeecd7e21c2c1ebe944ebecd594615d76
Georgi Georgiev <g.georgiev@softuni.bg>
2017-09-27 15:11:31 +0300

    Renamed and moved service following the new conventions https://github.com/SoftUni-Internal/suls-issues/issues/3159

---
+++ Open Judge System/OJS.Services.Common/BackgroundJobs/IHangfireBackgroundJobService.cs
@@ -0,0 +1,12 @@
+﻿namespace OJS.Services.Common.BackgroundJobs
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public interface IHangfireBackgroundJobService : IService
+    {
+       void AddOrUpdateRecurringJob(object recurringJobId, Expression<Action> methodCall, string cronExpression);
+
+       void AddOrUpdateRecurringJob<T>(object recurringJobId, Expression<Action<T>> methodCall, string cronExpression);
+    }
+}
