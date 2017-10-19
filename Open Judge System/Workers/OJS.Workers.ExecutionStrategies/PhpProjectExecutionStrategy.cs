f9eceb0642b507219918e6755ba9bc8cd70cf820
jicata <svetlin.galov@gmail.com>
2017-10-19 16:35:30 +0300

    Added logic for executing tests closes https://github.com/SoftUni-Internal/suls-issues/issues/3341

--- Open Judge System/Workers/OJS.Workers.ExecutionStrategies/PhpProjectExecutionStrategy.cs
+++ Open Judge System/Workers/OJS.Workers.ExecutionStrategies/PhpProjectExecutionStrategy.cs
@@ -1,9 +1,12 @@
 ﻿namespace OJS.Workers.ExecutionStrategies
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using OJS.Common;
     using OJS.Common.Extensions;
+    using OJS.Workers.Checkers;
+    using OJS.Workers.Executors;
 
     public class PhpProjectExecutionStrategy : ExecutionStrategy
     {
@@ -17,13 +20,15 @@
         {
             if (!File.Exists(phpCliExecutablePath))
             {
-                throw new ArgumentException($"PHP CLI not found in: {phpCliExecutablePath}", nameof(phpCliExecutablePath));
+                throw new ArgumentException(
+                    $"PHP CLI not found in: {phpCliExecutablePath}", 
+                    nameof(phpCliExecutablePath));
             }
 
             this.phpCliExecutablePath = phpCliExecutablePath;
         }
 
-        public string SuperGlobalsTemplatePath => $"{this.WorkingDirectory}\\${SuperGlobalsTemplateName}";
+        public string SuperGlobalsTemplatePath => $"{this.WorkingDirectory}\\{SuperGlobalsTemplateName}";
 
         public override ExecutionResult Execute(ExecutionContext executionContext)
         {
@@ -38,18 +43,42 @@
             FileHelpers.UnzipFile(submissionPath, this.WorkingDirectory);
             File.Delete(submissionPath);
 
-            string pathToSubmissionEntryPoint =
+            string submissionEntryPointPath =
                 FileHelpers.FindFileMatchingPattern(this.WorkingDirectory, SubmissionEntryPoint);
 
-            if (string.IsNullOrEmpty(pathToSubmissionEntryPoint))
+            if (string.IsNullOrEmpty(submissionEntryPointPath))
             {
                 throw new ArgumentException($"{SubmissionEntryPoint} not found in submission folder!");
             }
 
-            this.RequireSuperGlobalsTemplateInUserCode(pathToSubmissionEntryPoint);
+            this.RequireSuperGlobalsTemplateInUserCode(submissionEntryPointPath);
+
+            var checker = Checker.CreateChecker(
+                executionContext.CheckerAssemblyName, 
+                executionContext.CheckerTypeName, 
+                executionContext.CheckerParameter);
+
+            result.TestResults = new List<TestResult>();
+
+            var executor = new RestrictedProcessExecutor();
             foreach (var test in executionContext.Tests)
             {
                 File.WriteAllText(this.SuperGlobalsTemplatePath, test.Input);
+
+                var processExecutionResult = executor.Execute(
+                    this.phpCliExecutablePath,
+                    string.Empty,
+                    executionContext.TimeLimit,
+                    executionContext.MemoryLimit,
+                    new[] { submissionEntryPointPath });
+
+                var testResult = this.ExecuteAndCheckTest(
+                    test, 
+                    processExecutionResult, 
+                    checker, 
+                    processExecutionResult.ReceivedOutput);
+
+                result.TestResults.Add(testResult);
             }
 
             return result;
