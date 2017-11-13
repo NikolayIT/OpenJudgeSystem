e2687315753497faa803bd45c6163f8e20ff3a5b
Georgi Georgiev <gogo4ds@gmail.com>
2017-11-13 12:02:23 +0200

    Added ParticipantsDataService https://github.com/SoftUni-Internal/suls-issues/issues/3445

---
+++ Open Judge System/OJS.Services/Data/Participants/IParticipantsDataService.cs
@@ -0,0 +1,14 @@
+﻿namespace OJS.Services.Data.Participants
+{
+    using System.Linq;
+
+    using OJS.Data.Models;
+    using OJS.Services.Common;
+
+    public interface IParticipantsDataService : IService
+    {
+        IQueryable<Participant> GetParticipantsWithSubmissions();
+
+        IQueryable<Participant> GetParticipantsWithParticipantScores();
+    }
+}
---
+++ Open Judge System/OJS.Services/Data/Participants/ParticipantsDataService.cs
@@ -0,0 +1,30 @@
+﻿namespace OJS.Services.Data.Participants
+{
+    using System.Data.Entity;
+    using System.Linq;
+
+    using OJS.Data.Models;
+    using OJS.Data.Repositories.Contracts;
+
+    public class ParticipantsDataService : IParticipantsDataService
+    {
+        private readonly IEfGenericRepository<Participant> participnats;
+
+        public ParticipantsDataService(IEfGenericRepository<Participant> participnats) =>
+            this.participnats = participnats;
+
+        public IQueryable<Participant> GetParticipantsWithSubmissions() =>
+            this.participnats
+                .All()
+                .Include(par => par.User)
+                .Include(par => par.Contest.Problems
+                    .Select(pr => pr.ParticipantScores
+                        .Select(sc => sc.Submission)));
+
+        public IQueryable<Participant> GetParticipantsWithParticipantScores() =>
+            this.participnats
+                .All()
+                .Include(par => par.User)
+                .Include(par => par.Contest.Problems.Select(pr => pr.ParticipantScores));
+    }
+}
