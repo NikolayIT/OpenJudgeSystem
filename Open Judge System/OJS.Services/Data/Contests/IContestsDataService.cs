3939897955feff0f72dea142b4d7bf202be9709f
Georgi Georgiev <gogo4ds@gmail.com>
2017-12-05 15:47:42 +0200

    Replaced call to Data with call to the service Closes https://github.com/SoftUni-Internal/suls-issues/issues/3639

--- Open Judge System/OJS.Services/Data/Contests/IContestsDataService.cs
+++ Open Judge System/OJS.Services/Data/Contests/IContestsDataService.cs
@@ -19,6 +19,8 @@
 
         IQueryable<Contest> GetAllVisible();
 
+        IQueryable<Contest> GetAllWithDeleted();
+
         void DeleteById(int contestId);
 
         bool CanBeCompetedById(int contestId);
--- Open Judge System/Web/OJS.Web/Controllers/TempController.cs
+++ Open Judge System/Web/OJS.Web/Controllers/TempController.cs
@@ -13,6 +13,7 @@
     using OJS.Data.Models;
     using OJS.Data.Repositories.Base;
     using OJS.Services.Common.BackgroundJobs;
+    using OJS.Services.Data.Contests;
     using OJS.Services.Data.SubmissionsForProcessing;
     using OJS.Web.Common.Attributes;
 
@@ -20,11 +21,16 @@
     public class TempController : BaseController
     {
         private readonly IHangfireBackgroundJobService backgroundJobs;
+        private readonly IContestsDataService contestsData;
 
-        public TempController(IOjsData data, IHangfireBackgroundJobService backgroundJobs)
+        public TempController(
+            IOjsData data,
+            IHangfireBackgroundJobService backgroundJobs,
+            IContestsDataService contestsData)
             : base(data)
         {
             this.backgroundJobs = backgroundJobs;
+            this.contestsData = contestsData;
         }
 
         public ActionResult RegisterJobForCleaningSubmissionsForProcessingTable()
@@ -93,8 +99,8 @@
 
         // TODO: Remove this method after updating the entities
         public void MigrateAllExistingLabsToNewEnumValue() =>
-            this.Data.Contests
-                .AllWithDeleted()
+            this.contestsData
+                .GetAllWithDeleted()
                 .Where(c => c.Type == (ContestType)2)
                 .Update(c => new Contest { Type = ContestType.Lab });
     }
