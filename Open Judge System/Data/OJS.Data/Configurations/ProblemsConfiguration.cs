945c226208f8aeb9dc1aa156f6a0d33cc25b3e05
Georgi Georgiev <gogo4ds@gmail.com>
2017-12-08 14:13:49 +0200

    Added properties to models and modelbuilder configuration Closes https://github.com/SoftUni-Internal/suls-issues/issues/3664

---
+++ Open Judge System/Data/OJS.Data/Configurations/ProblemsConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace OJS.Data.Configurations
+{
+    using System.Data.Entity.ModelConfiguration;
+
+    using OJS.Data.Models;
+
+    public class ProblemsConfiguration : EntityTypeConfiguration<Problem>
+    {
+        public ProblemsConfiguration()
+        {
+            /*
+             * The following configuration fixes:
+             * Introducing FOREIGN KEY constraint 'FK_dbo.Problems_dbo.Contests_ContestId' on table 'Problems' may cause cycles or multiple cascade paths. Specify ON DELETE NO ACTION or ON UPDATE NO ACTION, or modify other FOREIGN KEY constraints.
+             */
+            this.HasRequired(p => p.Contest)
+                .WithMany(c => c.Problems)
+                .HasForeignKey(p => p.ContestId)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
