78b0ce08661fe8bda23b63ad3797034c4bb00752
Georgi Georgiev <gogo4ds@gmail.com>
2017-12-20 15:26:54 +0200

    Modified Total cell title to show max points if Online Contest Closes https://github.com/SoftUni-Internal/suls-issues/issues/3721

--- Open Judge System/Data/OJS.Data.Models/Contest.cs
+++ Open Judge System/Data/OJS.Data.Models/Contest.cs
@@ -201,6 +201,9 @@
         [NotMapped]
         public bool HasPracticePassword => this.PracticePassword != null;
 
+        [NotMapped]
+        public bool IsOnline => this.Type == ContestType.OnlinePractialExam;
+
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             var validationResults = new List<ValidationResult>();
