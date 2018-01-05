210097d21dc876f37510bbec84418e60dd72f34a
Viktor Kazakov <victor.d.kazakov@gmail.com>
2018-01-05 22:27:26 +0200

    Fixed query to correctly display non-affected participants when subtracting time.
    Closes https://github.com/SoftUni-Internal/suls-issues/issues/3770

--- Open Judge System/OJS.Services/Business/Participants/IParticipantsBusinessService.cs
+++ Open Judge System/OJS.Services/Business/Participants/IParticipantsBusinessService.cs
@@ -15,7 +15,7 @@
             bool isAdmin);
 
         /// <summary>
-        /// Updates constest duration for participants in contest, 
+        /// Updates contest duration for participants in contest, 
         /// in time range with amount of minutes provided. If any participants' contest duration
         /// would be reduced below the base contest duration they are not updated.
         /// </summary>
@@ -24,10 +24,25 @@
         /// Amount can be negative</param>
         /// <param name="contestStartTimeRangeStart">The lower bound against which participants' contest start time would be checked</param>
         /// <param name="contestStartTimeRangeEnd">The upper bound against which participants' contest start time would be checked</param>
-        /// <returns>Returns participants who were not updated
-        /// because their contest duration would be reduced below
-        /// the base contest duration</returns>
-        IQueryable<Participant> UpdateContestEndTimeForAllParticipantsByContestByParticipantContestStartTimeRangeAndTimeIntervalInMinutes(
+        void UpdateContestEndTimeForAllParticipantsByContestByParticipantContestStartTimeRangeAndTimeIntervalInMinutes(
+            int contestId,
+            int minutes,
+            DateTime contestStartTimeRangeStart,
+            DateTime contestStartTimeRangeEnd);
+
+        /// <summary>
+        /// Filters participants whose contest duration would fall below
+        /// the base duration of the contest if the amount of minutes provided
+        /// is added to their Contest End Time.
+        /// </summary>
+        /// <param name="contestId">The id of the contest</param>
+        /// <param name="minutes">Amount of minutes to be added to the participant's contest end time. 
+        /// Amount can be negative</param>
+        /// <param name="contestStartTimeRangeStart">The lower bound against which participants' contest start time would be checked</param>
+        /// <param name="contestStartTimeRangeEnd">The upper bound against which participants' contest start time would be checked</param>
+        /// <returns>Returns participants whose contest duration would be reduced below
+        /// the base contest duration.</returns>
+        IQueryable<Participant> GetAllParticipantsWhoWouldBeReducedBelowDefaultContestDuration(
             int contestId,
             int minutes,
             DateTime contestStartTimeRangeStart,
