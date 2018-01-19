cdc19aa2feb334e1389cf6c632b5e2b1ea0def42
Georgi Georgiev <gogo4ds@gmail.com>
2018-01-19 16:35:10 +0200

    Added Exam Groups controller https://github.com/SoftUni-Internal/suls-issues/issues/3814

--- Open Judge System/Web/OJS.Web/App_GlobalResources/Views/Shared/Layout.Designer.cs
+++ Open Judge System/Web/OJS.Web/App_GlobalResources/Views/Shared/Layout.Designer.cs
@@ -19,7 +19,7 @@ namespace Resources.Views.Shared {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Layout {
@@ -138,6 +138,15 @@ namespace Resources.Views.Shared {
         public static string Cookies_notification_OK {
             get {
                 return ResourceManager.GetString("Cookies_notification_OK", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Exam groups.
+        /// </summary>
+        public static string Exam_groups {
+            get {
+                return ResourceManager.GetString("Exam_groups", resourceCulture);
             }
         }
         
---
+++ Open Judge System/Web/OJS.Web/Areas/Administration/Controllers/ExamGroupsController.cs
@@ -0,0 +1,20 @@
+﻿namespace OJS.Web.Areas.Administration.Controllers
+{
+    using System.Web.Mvc;
+
+    using OJS.Data;
+    using OJS.Web.Areas.Administration.Controllers.Common;
+
+    public class ExamGroupsController : LecturerBaseController
+    {
+        public ExamGroupsController(IOjsData data)
+            : base(data)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
