10af743e65cd9b2c3f8c6dc7da470dda86a669f7
Georgi Georgiev <gogo4ds@gmail.com>
2018-02-01 16:34:29 +0200

    Added custom execution logic to DotNetCoreCompileExecuteAndCheckExecutionStrategy https://github.com/SoftUni-Internal/suls-issues/issues/3895

--- Open Judge System/Workers/OJS.Workers.ExecutionStrategies/DotNetCoreCompileExecuteAndCheckExecutionStrategy.cs
+++ Open Judge System/Workers/OJS.Workers.ExecutionStrategies/DotNetCoreCompileExecuteAndCheckExecutionStrategy.cs
@@ -1,16 +1,104 @@
 ﻿namespace OJS.Workers.ExecutionStrategies
 {
     using System;
+    using System.IO;
+    using System.Linq;
+
     using OJS.Common.Models;
+    using OJS.Workers.Checkers;
+    using OJS.Workers.Common;
+    using OJS.Workers.Executors;
 
     public class DotNetCoreCompileExecuteAndCheckExecutionStrategy : CompileExecuteAndCheckExecutionStrategy
     {
+        private const string RuntimeConfigJsonTemplate = @"
+            {
+	            ""runtimeOptions"": {
+                    ""framework"": {
+                        ""name"": ""Microsoft.NETCore.App"",
+                        ""version"": ""2.0.0""
+                    }
+                }
+            }";
+
         public DotNetCoreCompileExecuteAndCheckExecutionStrategy(
             Func<CompilerType, string> getCompilerPathFunc,
             int baseTimeUsed,
             int baseMemoryUsed)
             : base(getCompilerPathFunc, baseTimeUsed, baseMemoryUsed)
         {
+        }
+
+        public override ExecutionResult Execute(ExecutionContext executionContext)
+        {
+            var result = new ExecutionResult();
+
+            // Compile the file
+            var compilerResult = this.ExecuteCompiling(executionContext, this.GetCompilerPathFunc, result);
+            if (!compilerResult.IsCompiledSuccessfully)
+            {
+                return result;
+            }
+
+            // Execute and check each test
+            var executor = new RestrictedProcessExecutor(this.BaseTimeUsed, this.BaseMemoryUsed);
+
+            var checker = Checker.CreateChecker(
+                executionContext.CheckerAssemblyName,
+                executionContext.CheckerTypeName,
+                executionContext.CheckerParameter);
+
+            var arguments = new[]
+            {
+                compilerResult.OutputFile
+            };
+
+            var compilerPath = this.GetCompilerPathFunc(executionContext.CompilerType);
+
+            foreach (var test in executionContext.Tests)
+            {
+                var processExecutionResult = executor.Execute(
+                    compilerPath,
+                    test.Input,
+                    executionContext.TimeLimit,
+                    executionContext.MemoryLimit,
+                    arguments,
+                    this.WorkingDirectory);
+
+                var testResult = this.ExecuteAndCheckTest(
+                    test,
+                    processExecutionResult,
+                    checker,
+                    processExecutionResult.ReceivedOutput);
+
+                result.TestResults.Add(testResult);
+            }
+
+            return result;
+        }
+
+        protected override CompileResult Compile(
+            CompilerType compilerType,
+            string compilerPath,
+            string compilerArguments,
+            string submissionFilePath)
+        {
+            this.CreateRuntimeConfigJsonFile(this.WorkingDirectory, RuntimeConfigJsonTemplate);
+            return base.Compile(compilerType, compilerPath, compilerArguments, submissionFilePath);
+        }
+
+        private void CreateRuntimeConfigJsonFile(string directory, string text)
+        {
+            var userSubmissionFileName = Directory
+                .GetFiles(directory)
+                .Select(Path.GetFileName)
+                .First();
+
+            var jsonFileName = $"{userSubmissionFileName}.runtimeconfig.json";
+
+            var jsonFilePath = Path.Combine(directory, jsonFileName);
+
+            File.WriteAllText(jsonFilePath, text);
         }
     }
 }
