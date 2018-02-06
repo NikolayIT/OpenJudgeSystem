b8706cc17ade01d358e747fb9d504f31b51f0e1f
Georgi Georgiev <gogo4ds@gmail.com>
2018-02-06 18:47:23 +0200

    Changed sdk target version for DotNetCoreCompileExecuteAndCheckExecutionStrategy https://github.com/SoftUni-Internal/suls-issues/issues/3895

--- Open Judge System/Workers/OJS.Workers.ExecutionStrategies/DotNetCoreCompileExecuteAndCheckExecutionStrategy.cs
+++ Open Judge System/Workers/OJS.Workers.ExecutionStrategies/DotNetCoreCompileExecuteAndCheckExecutionStrategy.cs
@@ -16,7 +16,7 @@
 	            ""runtimeOptions"": {
                     ""framework"": {
                         ""name"": ""Microsoft.NETCore.App"",
-                        ""version"": ""2.0.0""
+                        ""version"": ""2.0.5""
                     }
                 }
             }";
