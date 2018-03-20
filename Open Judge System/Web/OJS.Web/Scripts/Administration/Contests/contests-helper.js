f744b716f14426d51dcd90904db552d79af3654f
Georgi Georgiev <gogo4ds@gmail.com>
2018-03-20 15:09:50 +0200

    Added extra check in makeContestNameLinkToDetailsPage in Contests/contests-helper.js

--- Open Judge System/Web/OJS.Web/Scripts/Administration/Contests/contests-helper.js
+++ Open Judge System/Web/OJS.Web/Scripts/Administration/Contests/contests-helper.js
@@ -2,6 +2,10 @@
     var contestId = data.ContestId || data.Id;
     var contestName = data.ContestName || data.Name;
 
+    if (typeof contestName == typeof undefined) {
+        return null;
+    }
+
     var nameUrl = contestName
         .replace('C#', 'CSharp')
         .replace('C++', 'CPlusPlus')
