fe474897f36750cb0f3af690d35598f28f21240c
Georgi Georgiev <gogo4ds@gmail.com>
2018-04-02 12:42:33 +0300

    Removed BasicSubmissions.cshtml Closes https://github.com/SoftUni-Internal/suls-issues/issues/4233

--- Open Judge System/Web/OJS.Web/Controllers/SubmissionsController.cs
+++ Open Judge System/Web/OJS.Web/Controllers/SubmissionsController.cs
@@ -10,8 +10,10 @@
     using Newtonsoft.Json;
 
     using OJS.Common;
+    using OJS.Common.Models;
     using OJS.Data;
     using OJS.Data.Models;
+    using OJS.Web.Common.Attributes;
     using OJS.Web.Common.Extensions;
     using OJS.Web.ViewModels.Submission;
 
@@ -22,28 +24,18 @@
         {
         }
 
-        [HttpGet]
+        [AuthorizeRoles(SystemRole.Administrator, SystemRole.Lecturer)]
         public ActionResult Index()
         {
-            if (this.User.IsLoggedIn())
+            if (this.User.IsAdmin())
             {
-                if (this.User.IsAdmin())
-                {
-                    this.ViewBag.SubmissionsInQueue = this.Data.Submissions.All().Count(x => !x.Processed);
-                }
-
-                return this.View("AdvancedSubmissions");
+                this.ViewBag.SubmissionsInQueue = this.Data.Submissions.All().Count(x => !x.Processed);
             }
 
-            var submissions = this.Data.Submissions
-                .GetLastFiftySubmissions()
-                .Select(SubmissionViewModel.FromSubmission)
-                .ToList();
-
-            return this.View("BasicSubmissions", submissions);
+            return this.View("AdvancedSubmissions");
         }
 
-        [Authorize]
+        [AuthorizeRoles(SystemRole.Administrator, SystemRole.Lecturer)]
         public ActionResult GetSubmissionsGrid(
             bool notProcessedOnly = false,
             string userId = null,
