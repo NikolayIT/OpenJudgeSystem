95a4e79b3434c92db050f48be15861fbeea07ec1
Georgi Georgiev <gogo4ds@gmail.com>
2018-06-05 12:54:03 +0300

    CR Fixes: fixed grammar error in message https://github.com/SoftUni-Internal/suls-issues/issues/4459

--- Open Judge System/Web/OJS.Web/App_GlobalResources/Areas/Contests/Views/Submissions/SubmissionsView.Designer.cs
+++ Open Judge System/Web/OJS.Web/App_GlobalResources/Areas/Contests/Views/Submissions/SubmissionsView.Designer.cs
@@ -313,7 +313,7 @@ namespace Resources.Areas.Contests.Views {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to The process executing your submission for this test may not received the output successfully. Please try to submit again the same solution. If the result does not change, then search the error in the submission itself..
+        ///   Looks up a localized string similar to The process executing your submission for this test may not have received the output successfully. Please try to submit again the same solution. If the result does not change, then search the error in the submission itself..
         /// </summary>
         public static string Try_submit_again_message_when_no_output {
             get {
