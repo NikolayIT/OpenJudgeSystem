045c3c38e83e8d3eeb444fc5ddc10aa18319f1ab
Georgi Georgiev <gogo4ds@gmail.com>
2018-06-27 18:39:25 +0300

    Implemented timer that triggers every minute to check the local worker service https://github.com/SoftUni-Internal/suls-issues/issues/1811

--- Open Judge System/Workers/OJS.Workers.LocalWorker/Batch Files/InstallService.bat
+++ Open Judge System/Workers/OJS.Workers.LocalWorker/Batch Files/InstallService.bat
@@ -5,4 +5,21 @@ CD %~dp0
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\installutil "..\OJS.Workers.LocalWorker.exe"
 sc failure "OJS Local Worker Service" actions= restart/60000/restart/60000/""/60000 reset= 7200
 NET START "OJS Local Worker Service"
+
+@echo off
+SC QUERY "OJS Local Worker Monitoring Service" > NUL
+IF ERRORLEVEL 1060 GOTO MISSING
+	for /F "tokens=3 delims=: " %%H in ('sc query "OJS Local Worker Monitoring Service" ^| findstr "STATE"') do (
+	  if /I "%%H" NEQ "RUNNING" (
+		NET START "OJS Local Worker Monitoring Service"
+	  )
+	)
+GOTO END
+
+:MISSING
+	CD %~dp0
+	C:\Windows\Microsoft.NET\Framework\v4.0.30319\installutil "..\..\..\..\OJS.Workers.LocalWorkerMonitoring\bin\Debug\OJS.Workers.LocalWorkerMonitoring.exe"
+	NET START "OJS Local Worker Monitoring Service"
+:END
+
 pause
--- Open Judge System/Workers/OJS.Workers.LocalWorkerMonitoring/LocalWorkerMonitoringService.cs
+++ Open Judge System/Workers/OJS.Workers.LocalWorkerMonitoring/LocalWorkerMonitoringService.cs
@@ -1,6 +1,8 @@
 ﻿namespace OJS.Workers.LocalWorkerMonitoring
 {
+    using System;
     using System.ServiceProcess;
+    using System.Timers;
 
     using log4net;
 
@@ -21,16 +23,42 @@
         protected override void OnStart(string[] args)
         {
             logger.Info($"{nameof(LocalWorkerMonitoringService)} starting...");
-            logger.Info($"{Constants.LocalWorkerServiceName} status is: {this.localWorkerServiceController.Status}");
+
+            // Set up a timer to trigger every minute.
+            var timer = new Timer { Interval = 60000 };
+            timer.Elapsed += this.OnTimer;
+            timer.Start();
 
             logger.Info($"{nameof(LocalWorkerMonitoringService)} started.");
         }
 
         protected override void OnStop()
         {
-            logger.Info($"{nameof(LocalWorkerMonitoringService)} stopping...");
-
             logger.Info($"{nameof(LocalWorkerMonitoringService)} stopped.");
+        }
+
+        private void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            var localWorkerServiceStatus = this.localWorkerServiceController.Status;
+
+            if (localWorkerServiceStatus == ServiceControllerStatus.StopPending)
+            {
+                logger.Warn($"{Constants.LocalWorkerServiceName} is stopping...");
+            }
+            else if (localWorkerServiceStatus == ServiceControllerStatus.Stopped)
+            {
+                logger.Warn($"{Constants.LocalWorkerServiceName} has stopped.");
+
+                try
+                {
+                    this.localWorkerServiceController.Start();
+                    logger.Info($"{Constants.LocalWorkerServiceName} has started successfully.");
+                }
+                catch (Exception ex)
+                {
+                    logger.Error($"Restarting the {Constants.LocalWorkerServiceName} has thrown an exception: {ex.Message}");
+                }
+            }
         }
     }
 }
