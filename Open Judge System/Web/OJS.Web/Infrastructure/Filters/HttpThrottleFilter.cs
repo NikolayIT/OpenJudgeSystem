b348f3b52d40676c885c8521fa27ebc36de9eac3
achobanov <alexchobanov@gmail.com>
2019-10-07 11:56:06 +0300

    Created HttpThrottleFilter.cs https://github.com/SoftUni-Internal/suls-issues/issues/5559

---
+++ Open Judge System/Web/OJS.Web/Infrastructure/Filters/HttpThrottleFilter.cs
@@ -0,0 +1,25 @@
+﻿namespace Suls.Web.Common.Filters
+{
+    using System.Linq;
+
+    using MvcThrottle;
+
+    public class HttpThrottleFilter : ThrottlingFilter
+    {
+        public HttpThrottleFilter(
+            int limitPerSecond,
+            int limitPerMinutre,
+            int limitPerHour,
+            int limitPerDay,
+            string[] whiteListRange) =>
+            new ThrottlingFilter()
+            {
+                Policy = new ThrottlePolicy(limitPerSecond, limitPerMinutre, limitPerHour, limitPerDay)
+                {
+                    IpThrottling = true,
+                    IpWhitelist = whiteListRange.ToList(),
+                },
+                Repository = new CacheRepository(),
+            };
+    }
+}
