b849811e18b6ec04c94012fe078fc54881b12b9f
gogo4ds <gogo4ds@gmail.com>
2020-03-25 10:26:15 +0200

    Added default migration for PythonProjectUnitTestsExecutionStrategy
    https://github.com/SoftUni-Internal/suls-issues/issues/5798

--- Open Judge System/Data/OJS.Data/Migrations/DefaultMigrationConfiguration.cs
+++ Open Judge System/Data/OJS.Data/Migrations/DefaultMigrationConfiguration.cs
@@ -283,6 +283,16 @@
                 },
                 new SubmissionType
                 {
+                    Name = "Python Project Unit Tests (unittest)",
+                    CompilerType = CompilerType.None,
+                    AdditionalCompilerArguments = string.Empty,
+                    ExecutionStrategyType = ExecutionStrategyType.PythonProjectUnitTests,
+                    IsSelectedByDefault = false,
+                    AllowedFileExtensions = "zip",
+                    AllowBinaryFilesUpload = true,
+                },
+                new SubmissionType
+                {
                     Name = "PHP code (CGI)",
                     CompilerType = CompilerType.None,
                     AdditionalCompilerArguments = string.Empty,
